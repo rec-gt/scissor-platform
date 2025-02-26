70a32d18f13a11125741dee6eaa0bbafd0a8ac2f
t <a@a.a>
2025-02-26 14:53:46 +0800

    feat: update

--- Main/DetectSystem.h
+++ Main/DetectSystem.h
@@ -19,10 +19,10 @@ public:
 
     switch (status) {
       case SYS_RUNNING:
-        displayOLED.print("", "系統運作中", "", 2);
+        displayOLED.print("", "系統運作中", "", SYS_RUNNING);
         break;
       case SYS_STOPPED:
-        displayOLED.print("偵測到障礙物", "系統暫停運作", "", 3);
+        displayOLED.print("偵測到障礙物", "系統暫停運作", "", SYS_STOPPED);
         break;
     }
 
--- Main/DisplayOLED.h
+++ Main/DisplayOLED.h
@@ -6,6 +6,7 @@
 
 #ifndef displayOLED_h
 #define displayOLED_h
+
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 class DisplayOLED {

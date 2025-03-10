bba206188bb28f2f6c2913a36cef70cd61b82a16
t <a@a.a>
2025-03-10 17:33:00 +0800

    feat: update

--- Main/LaserSensor.h
+++ Main/LaserSensor.h
@@ -215,10 +215,9 @@ public:
   }
 
   byte getSensors2Status() {
-    byte res = 252;
-
+    byte res = 0;
     for (int i = 10; i >= 8; i--) {
-      if (this->laserSensors[i].isDetected()) { res -= pow(2, (i - 8)); }
+      if (this->laserSensors[i].isDetected()) { res += pow(2, (i - 8)); }
     }
     return res;
   }

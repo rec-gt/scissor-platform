3ade40806e46bbe1517a99cbeabc1b13cd4b4fe7
t <a@a.a>
2025-03-13 14:25:53 +0800

    feat: update

--- Main/Main.ino
+++ Main/Main.ino
@@ -35,16 +35,16 @@ NBIoT nbiot;
 
 LaserSensor sensors[] = {
   // (longer threshold, shorter threshold, fine tune)
-  LaserSensor(A0, 1200, 800, 203),  // 工廠fine-tune，遮擋鏡頭做測試
-  // LaserSensor(A1, 800, 500, 216),
-  // LaserSensor(A2, 800, 500, 215),
-  // LaserSensor(A3, 800, 500, 232),
-  // LaserSensor(A4, 800, 500, 222),
-  // LaserSensor(A5, 800, 500, 216),
-  // LaserSensor(A6, 800, 500, 234),
-  // LaserSensor(A7, 800, 500, 229),
-  // LaserSensor(A8, 800, 500, 227),
-  // LaserSensor(A9, 800, 500, 222),
+  LaserSensor(A0, 800, 500, 203),  // 工廠fine-tune，遮擋鏡頭做測試
+  LaserSensor(A1, 800, 500, 216),
+  LaserSensor(A2, 800, 500, 215),
+  LaserSensor(A3, 800, 500, 232),
+  LaserSensor(A4, 800, 500, 222),
+  LaserSensor(A5, 800, 500, 216),
+  LaserSensor(A6, 800, 500, 234),
+  LaserSensor(A7, 800, 500, 229),
+  LaserSensor(A8, 800, 500, 227),
+  LaserSensor(A9, 800, 500, 222),
 };
 
 LaserSensorManager sensorsManager(sensors, sizeof(sensors) / sizeof(sensors[0]));

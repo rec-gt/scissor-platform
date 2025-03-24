cd05f837d0bcff206e87c8ab3dbd3e896d8177c9
t <a@a.a>
2025-03-24 11:16:34 +0800

    feat: update

--- test_RS485_master/test_RS485_master.ino
+++ test_RS485_master/test_RS485_master.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+#define RS485_SERIAL Serial3
+#define RS485_CTRL_PIN 31
 
+void setup() {
+  Serial.begin(9600);
+  RS485_SERIAL.begin(9600);
+  pinMode(RS485_CTRL_PIN, OUTPUT);
+  digitalWrite(RS485_CTRL_PIN, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  RS485_SERIAL.print("HI");
+  delay(1000);
 }
--- test_RS485_slave/test_RS485_slave.ino
+++ test_RS485_slave/test_RS485_slave.ino
@@ -1,9 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#define RS485_Serial Serial3
+#define RS485_CTRL_PIN 31
 
+
+String readString;
+
+void setup() {
+  Serial.begin(9600);
+  RS485_Serial.begin(9600);
+  pinMode(RS485_CTRL_PIN, OUTPUT);
+  digitalWrite(RS485_CTRL_PIN, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  listenReceived();
+  Serial.println(readString);
+  Serial.println("------");
+  delay(500);
+}
 
+void listenReceived() {
+  readString = "";
+  while (RS485_Serial.available()) {
+    readString += (char)RS485_Serial.read();
+  }
 }

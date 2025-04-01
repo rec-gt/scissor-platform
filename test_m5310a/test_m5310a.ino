82e874a1638a38422850dbf909ffcb6b7d8d3082
t <a@a.a>
2025-04-01 11:40:25 +0800

    feat: update

--- test_m5310a/test_m5310a.ino
+++ test_m5310a/test_m5310a.ino
@@ -10,6 +10,6 @@ void setup() {
 }
 
 void loop() {
-  nbiot.sendCMDFast("AT+MQTTPUB=\"rgt/869976034806621/in\",1,0,0,0,\"{\"seq\":1,\"csq\":25,\"sw\":0,\"din\":" + String(255) + ",\"dout\":" + String(255) + ",\"ain\":[0,0,0,0],\"aout\":[0,0,0,0]}\"");
+  nbiot.sendCMDFast("AT+MQTTPUB=\"rgt/869976034840018/in\",1,0,0,0,\"{\"seq\":1,\"csq\":25,\"sw\":0,\"din\":" + String(255) + ",\"dout\":" + String(255) + ",\"ain\":[0,0,0,0],\"aout\":[0,0,0,0]}\"");
   delay(30 * 1000);
 }

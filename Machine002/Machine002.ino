13d1331138818176f4b8fe32db6249a8a2592438
t <a@a.a>
2025-04-16 16:25:30 +0800

    feat: update

--- Machine002/Machine002.ino
+++ Machine002/Machine002.ino
@@ -25,7 +25,7 @@ BaseThresholdSwitch baseThresholdSwitch(6);
 
 Relay relay(24);
 
-Light tenSecondLight(25);
+Light tenSecondsLight(25);
 
 Light powerLight(26);
 
@@ -39,8 +39,8 @@ NBIoT nbiot;
 
 LaserSensor sensors[] = {
   // (pin, longer threshold, shorter threshold, fine tune)
-  LaserSensor(A0, 800, 500, 220),  // 工廠fine-tune，遮擋鏡頭做測試
-  LaserSensor(A1, 800, 500, 282),
+  LaserSensor(A0, 800, 500, 330),  // 工廠fine-tune，遮擋鏡頭做測試
+  LaserSensor(A1, 800, 500, 330),
   LaserSensor(A2, 800, 500, 330),
   LaserSensor(A3, 800, 500, 330),
   LaserSensor(A4, 800, 500, 330),
@@ -61,40 +61,39 @@ RS485Module rs485;
 void setup() {
   Serial.begin(9600);
 
-  powerLight.off();
-  tenSecondLight.off();
+  // === System Starting ===
+  relay.cut();
+  tenSecondsLight.off();
   trafficLight.off();
   warningSystem.off();
-  relay.cut();
-
   displayOLED.init();
-
   nbiot.init();
-
   detectSystem.set(SYS_RUNNING);
-
   powerLight.on();
+
+  delay(500);
 }
 
 void loop() {
-  // ========= handling press button =========
+  // === handling NB-IoT ===
+  // nbiot.publish(sensorsManager.getSensors8Status(), sensorsManager.getSensors2Status(), detectSystem.getStatus(), 0);
+  nbiot.publish();
+
+  // === handling press button ===
   pressButton.listen();
 
-
-  // ========= handling threshold switch =========
+  // === handling threshold switch ===
   baseThresholdSwitch.listen();
   sensorsManager.changeAllBaseThreshold(baseThresholdSwitch.on());
 
-
-  // ========= controlling traffic light =========
+  // === controlling traffic light ===
   trafficLight.listen(sensorsManager.getMinDistance());
 
-
-  // ========= controlling detection system =========
+  // === controlling detection system ===
   if (detectSystem.is(SYS_RUNNING)) {
     relay.connect();
     warningSystem.off();
-    tenSecondLight.off();
+    tenSecondsLight.off();
 
     if (sensorsManager.isOneDetected()) {
       detectSystem.set(SYS_STOPPED);
@@ -103,10 +102,11 @@ void loop() {
     if (!sensorsManager.areAllHealthy()) {
       detectSystem.set(SYS_FAILURE);
     }
+
   } else if (detectSystem.is(SYS_STOPPED)) {
     relay.cut();
     warningSystem.on();
-    tenSecondLight.on();
+    tenSecondsLight.on();
 
     if (sensorsManager.areAllEscaped()) {  // 1. sensor keep detection, once escape from obstacle, switch to RUNNING
       detectSystem.set(SYS_RUNNING);
@@ -116,24 +116,26 @@ void loop() {
       detectSystem.set(SYS_ALLOW_10S);
       countdownTimer.set();
     }
+
   } else if (detectSystem.is(SYS_ALLOW_10S)) {
     relay.connect();
     warningSystem.off();
-    tenSecondLight.off();
+    tenSecondsLight.off();
+
     countdownTimer.countdown(countDownCallback);
+
   } else if (detectSystem.is(SYS_FAILURE)) {
     relay.cut();
     warningSystem.on();
+
     if (sensorsManager.areAllHealthy()) {
       detectSystem.set(SYS_RUNNING);
     }
   }
 
-  // ========= NB-IoT =========
-  // nbiot.publish(sensorsManager.getSensors8Status(), sensorsManager.getSensors2Status(), detectSystem.getStatus(), 0);
-  nbiot.publish();
 
-  // ========= Debugging =========
+
+  // === Debugging ===
   // sensorsManager.printOne(0);
   // Serial.println();
   // sensorsManager.pr intAll();
--- Machine002/SystemEnums.h
+++ Machine002/SystemEnums.h
@@ -7,13 +7,14 @@
 
 enum SystemStatus {
   SYS_INIT,
+  SYS_STARTING,
   SYS_RUNNING,
   SYS_STOPPED,
   SYS_ALLOW_10S,
   SYS_FAILURE
 };
 
-const char* STATUS_STR[] = { "SYS_INIT", "SYS_RUNNING", "SYS_STOPPED", "SYS_ALLOW_10S", "SYS_FAILURE" };
+const char* STATUS_STR[] = { "SYS_INIT", "SYS_STARTING", "SYS_RUNNING", "SYS_STOPPED", "SYS_ALLOW_10S", "SYS_FAILURE" };
 
 enum TrafficStatus {
   TRAFFIC_OFF,

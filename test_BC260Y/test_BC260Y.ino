42155d565a7613925d1cd6bcb017c8be2124a94e
t <a@a.a>
2025-05-08 10:39:50 +0800

    feat: update

--- test_BC260Y/test_BC260Y.ino
+++ test_BC260Y/test_BC260Y.ino
@@ -2,13 +2,27 @@
 void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
+  // Serial1.print("AT");
 }
 
+// AT+CEREG?
+// AT+CGATT?
+// AT+CIMI
+// AT+CGSN=1
+// AT+QMTDISC=1
+// AT+QMTOPEN=0,"8.210.84.24",1880
+// AT+QMTCONN=0,"dev","tswh","1Wo=[6vA0m"
+// AT+QMTPUB=0,0,0,0,"rgt/861096060571706/in",29,"asd"
+
+
 void loop() {
+
   if (Serial1.available()) {
-    const int bufferSize = 64;
-    byte buffer[bufferSize];
-    int bytesRead = Serial1.readBytes(buffer, bufferSize);
-    Serial.println(bytesRead);
+    String str = Serial1.readString();
+    Serial.println(str);
+    if (str == "+QNBIOTEVENT: \"ENTER DEEPSLEEP\"") {
+      Serial.println("X");
+    }
   }
+  delay(1000);
 }

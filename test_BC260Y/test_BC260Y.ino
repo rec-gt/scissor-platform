bd1da083a4d5821253b65e7b7a28da73bd82c03e
t <a@a.a>
2025-05-09 12:17:40 +0800

    feat: update

--- test_BC260Y/test_BC260Y.ino
+++ test_BC260Y/test_BC260Y.ino
@@ -4,6 +4,8 @@ void setup() {
   Serial.begin(4800);
   NBIOT_Serial.begin(4800);
   Serial.println("=== START ===");
+  pinMode(6, OUTPUT);
+  digitalWrite(6, LOW);
 }
 
 // AT+CEREG?
@@ -26,21 +28,41 @@ void loop() {
     }
   }
 
-  if (NBIOT_Serial.available()) {
-    String res = NBIOT_Serial.readString();
-    Serial.println(res);
-  }
+  // if (NBIOT_Serial.available()) {
+  //   String res = NBIOT_Serial.readString();
+  //   Serial.println(res);
+  // }
 }
 
 
 void sendMsg() {
-  // wakeup();…
+  // wakeup();
   NBIOT_Serial.println("AT");
-  delay(300);
+  delay(3000);
+
+  if (NBIOT_Serial.available()) {
+    String res = NBIOT_Serial.readString();
+    Serial.println(res);
+  }
   NBIOT_Serial.println("AT+QMTOPEN=0,\"8.210.84.24\",1880");
-  delay(300);
+  delay(3000);
+
+  if (NBIOT_Serial.available()) {
+    String res = NBIOT_Serial.readString();
+    Serial.println(res);
+  }
   NBIOT_Serial.println("AT+QMTCONN=0,\"dev\",\"tswh\",\"1Wo=[6vA0m\"");
-  delay(300);
-  NBIOT_Serial.println("AT+QMTPUB=0,0,0,0,\"rgt/861096060571706\",1,\"d\"");
-  delay(300);
+  delay(3000);
+
+  if (NBIOT_Serial.available()) {
+    String res = NBIOT_Serial.readString();
+    Serial.println(res);
+  }
+  NBIOT_Serial.println("AT+QMTPUB=0,0,0,0,\"rgt/861096060571706\",1,\"i\"");
+  delay(3000);
+
+  if (NBIOT_Serial.available()) {
+    String res = NBIOT_Serial.readString();
+    Serial.println(res);
+  }
 }

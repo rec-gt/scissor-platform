8b5751211ff8404c0e0e2137d901ba4f5d2dac15
t <a@a.a>
2025-05-29 18:48:54 +0800

    feat: update

--- test_warning_system/NBIot.h
+++ test_warning_system/NBIot.h
@@ -89,13 +89,13 @@ public:
     // this->sendCMD("AT+CFUN=0", 100);
     // this->sendCMD("AT+QCSEARFCN", 3000);
     // this->sendCMD("AT+QRST=1", 3…000);
-    // this->sendCMD("AT+CFUN=1", 100);
+    this->sendCMD("AT+CFUN=1", 100);
     this->sendCMD("AT+QSCLK=0", 100);
     // this->sendCMD("AT+CPSMS=0", 100);
     // this->sendCMD("AT+CEDRXS=0,5", 100);
     // this->sendCMD("AT+CSCON=0", 100);
-    // this->sendCMD("AT+QMTCFG=version,0,1", 100);
-    // this->sendCMD("AT+QMTCFG=keepalive,0,0", 100);
+    // this->sendCMD("AT+QMTCFG=version,0,0", 100);
+    this->sendCMD("AT+QMTCFG=keepalive,0,60", 100);
     // this->sendCMD("AT+QMTCFG=session,0,1", 100);
     // this->sendCMD("AT+QIDNSCFG=0,8.8.8.8,1.1.1.1", 100);
 
@@ -184,7 +184,7 @@ public:
 
       this->sendCMD("AT+QMTCONN=0," + this->IMEI + ",tswh,1Wo=[6vA0m", 3000);
 
-      this->sendCMD("AT+QMTSUB=0,1,\"rgt/" + this->IMEI + "/in\",2", 3000);
+      this->sendCMD("AT+QMTSUB=0,1,\"rgt/" + this->IMEI + "/in\",0", 3000);
 
       if (this->isOK()) {
         break;
@@ -228,7 +228,7 @@ public:
         warningSystem.setIsActived(isActived > -1);
       }
 
-      this->clearBuffer();
+      // this->clearBuffer();
     }
   }
 

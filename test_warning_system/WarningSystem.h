cfa09acf7b90416ab3bb28752d82106c4fb4a08d
t <a@a.a>
2025-05-30 11:44:03 +0800

    feat: update

--- test_warning_system/WarningSystem.h
+++ test_warning_system/WarningSystem.h
@@ -13,19 +13,34 @@ private:
   SystemStatus currentStatus = DEFAULT;
 
   unsigned long prevMillis = 0;
+  unsigned long prevHeartBeatMillis = 0;
 
   byte recvStatus[_CODE_COUNT] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };  // 0=DEFAULT, 1=ISSUE, 2=CANCEL
   byte prevStatus[_CODE_COUNT] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };  // 0=IDLE_DEFAULT, 1=IDLE_BY_ISSUE, 2=IDLE_BY_CANCEL
+  byte issuePins[_CODE_COUNT] = { 10, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  byte issueDuration[_CODE_COUNT] = { 30, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  byte cancelPins[_CODE_COUNT] = { 11, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  byte cancelDuration[_CODE_COUNT] = { 30, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 public:
   void execAlarmTasks() {
     for (size_t i = 0; i < _CODE_COUNT; i++) {
+      // DEFAULE/CANCEL => ISSUE
       if (
         (this->prevStatus[i] == 0 || this->prevStatus[i] == 2)
         && this->recvStatus[i] == 1) {
         // 1. activate alarm
         // 2. update status
         prevStatus[i] = 1;
+      }
+      
+      // DEFAULE/ISSUE => CANCEL
+      if (
+        (this->prevStatus[i] == 0 || this->prevStatus[i] == 1)
+        && this->recvStatus[i] == 2) {
+        // 1. activate alarm
+        // 2. update status
+        prevStatus[i] = 2;
       }
     }
   }

2e447533fafb8dc6a9bf20549030df4c8bea7372
t <a@a.a>
2025-06-04 11:24:31 +0800

    feat: update

--- test_concurrent/AsyncSerial.h
+++ test_concurrent/AsyncSerial.h
@@ -1,10 +1,32 @@
 #ifndef AsyncSerial_h
 #define AsyncSerial_h
 
+#define NBIoT_Module Serial1
+
 class AsyncSerial {
 private:
+  String res = "";
 
+  void pruneResBuffer() {
+    this->res = "";
+  }
 public:
   AsyncSerial() {}
+
+  void waitMsg() {
+    if (NBIoT_Module.available() > 0) {
+      char byte = NBIoT_Module.read();
+      if (byte != '\r' && byte != '\n') {
+        this->res += byte;
+      }
+
+      if (byte == '\r') {
+        handleATCommand(res);
+        pruneResBuffer();
+      }
+    }
+  }
+
+
   ~AsyncSerial() {}
 };

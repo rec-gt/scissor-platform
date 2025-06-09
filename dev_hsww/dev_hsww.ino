df9fc55cb2d89530aca7cadc0a67011b64878966
t <a@a.a>
2025-06-09 10:28:32 +0800

    feat: update

---
+++ dev_hsww/dev_hsww.ino
@@ -0,0 +1,22 @@
+#include "NBIoT.h"
+
+NBIoT nbiot;
+
+String atCommand = "";
+
+void setup() {
+  Serial.begin(9600);
+  NBIoT_Serial.begin(9600);
+  delay(300);
+  // nbiot.init();
+  delay(300);
+}
+
+void otherJobs() {
+  Serial.println("doing other jobs...");
+}
+
+void loop() {
+  nbiot.listen();
+  nbiot.waitMsg();
+}

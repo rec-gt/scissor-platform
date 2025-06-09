f5807a65c793aed86dcbcb97c7f5d8b21f2c61f0
t <a@a.a>
2025-06-09 15:28:48 +0800

    feat: update

--- dev_hsww/dev_hsww.ino
+++ dev_hsww/dev_hsww.ino
@@ -4,16 +4,10 @@
 NBIoT nbiot;
 Speaker speaker;
 
-String atCommand = "";
-
 void setup() {
   Serial.begin(9600);
   NBIoT_Serial.begin(9600);
   delay(300);
-}
-
-void otherJobs() {
-  Serial.println("doing other jobs...");
 }
 
 void loop() {

3bcfe3bb99637b93a461c16eea3c459b48f6341e
t <a@a.a>
2025-06-30 16:30:30 +0800

    feat: update

--- demo_jp/ChargingSystem.h
+++ demo_jp/ChargingSystem.h
@@ -9,10 +9,10 @@
 #define ChargingSystem_h
 #define LoRaSerial Serial1
 
-#define SYS_RECOVERED 0
-#define SYS_STOPPED_BY_AMBIENT_TEMP 1
-#define SYS_STOPPED_BY_STATION_TEMP 2
-#define SYS_STOPPED_BY_CURRENT 3
+#define SIM_RECOVERED 0
+#define SIM_STOPPED_BY_AMBIENT_TEMP 1
+#define SIM_STOPPED_BY_STATION_TEMP 2
+#define SIM_STOPPED_BY_CURRENT 3
 
 // modules
 Relay relay(10);
@@ -31,11 +31,7 @@ private:
   int SPA = 500;          // set-point current
   byte C = 1;             // relay cut=0, connect=1
   byte M = MODE_RUNNING;  // system mode
-
-  byte status = STATUS_RUNNING;  // system status
-  byte attachedProblems = 0;     // problem buffer
-
-  unsigned long sendStatusMillis = millis();
+  byte simCache = 0;      // simulation only: problem buffer
 
 public:
   ChargingSystem(void){};
@@ -93,7 +89,7 @@ public:
         Serial.println(newModeStr);
         if (newModeStr == "0" || newModeStr == "1" || newModeStr == "2") {
           int newMode = newModeStr.toInt();
-          this->M = newMode;
+          this->setMode(newMode);
         }
       }
     }
@@ -103,69 +99,65 @@ public:
       int idx = utils.findStrIdx(this->recv, "ASK:");
       if (idx > -1) {
         delay(100);
+        this->collectData();
         this->sendStatus();
         delay(100);
       }
     }
   }
 
-  void listen() {
-    this->waitMsg();
-
-    thermometer1.listen();
-    thermometer2.listen();
-    ammeter.listen();
-
+  void collectData() {
     this->AT = thermometer1.get();
     this->ST = thermometer2.get();
     this->A = ammeter.get();
 
-    if (this->M == MODE_RUNNING) {
-      relay.connect();
+    if (this->modeIs(MODE_RUNNING)) {
       if (this->AT >= this->SPST || this->ST >= this->SPST || this->A >= this->SPA) {
-        this->M = MODE_STOPPED;
+        this->setMode(MODE_STOPPED);
       }
+      this->power(true);
+    } else if (this->modeIs(MODE_STOPPED)) {
+      this->power(false);
+    } else if (this->modeIs(MODE_BYPASS)) {
+      this->power(true);
+    } else if (this->modeIs(MODE_SIMULATION)) {
+      // error hook
+      if (this->AT >= this->SPST) {
+        this->simCache |= 1 << SIM_STOPPED_BY_AMBIENT_TEMP;
+      }
+      if (this->ST >= this->SPST) {
+        this->simCache |= 1 << SIM_STOPPED_BY_STATION_TEMP;
+      }
+      if (this->A >= this->SPA) {
+        this->simCache |= 1 << SIM_STOPPED_BY_CURRENT;
+      }
+      if (this->simCache == SIM_RECOVERED) {
+        this->power(true);
+      } else {
+        this->power(false);
 
-      // // error hook
-      // if (this->AT >= this->SPST) {
-      //   this->attachedProblems |= 1 << SYS_STOPPED_BY_AMBIENT_TEMP;
-      // }
-      // if (this->ST >= this->SPST) {
-      //   this->attachedProblems |= 1 << SYS_STOPPED_BY_STATION_TEMP;
-      // }
-      // if (this->A >= this->SPA) {
-      //   this->attachedProblems |= 1 << SYS_STOPPED_BY_CURRENT;
-      // }
-
-      // if (this->attachedProblems == SYS_RECOVERED) {
-      //   relay.connect();
-      //   this->C = 1;
-      // } else {
-      //   relay.cut();
-      //   this->C = 0;
-      //   this->A = 0;  // pseudo 0 current
-
-      //   // error recovery
-      //   if (this->AT < this->SPST - 50) {
-      //     this->attachedProblems &= ~(1 << SYS_STOPPED_BY_AMBIENT_TEMP);
-      //   }
-      //   if (this->ST < this->SPST - 50) {
-      //     this->attachedProblems &= ~(1 << SYS_STOPPED_BY_STATION_TEMP);
-      //   }
-      //   if (this->A < this->SPA - 5) {
-      //     this->attachedProblems &= ~(1 << SYS_STOPPED_BY_CURRENT);
-      //   }
-      // }
-    } else if (this->M == MODE_STOPPED) {
-      relay.cut();
-    } else if (this->M == MODE_BYPASS) {
-      relay.connect();
+        // error recovery
+        if (this->AT < this->SPST) {
+          this->simCache &= ~(1 << SIM_STOPPED_BY_AMBIENT_TEMP);
+        }
+        if (this->ST < this->SPST) {
+          this->simCache &= ~(1 << SIM_STOPPED_BY_STATION_TEMP);
+        }
+        if (this->A < this->SPA) {
+          this->simCache &= ~(1 << SIM_STOPPED_BY_CURRENT);
+        }
+      }
     }
   }
 
-  void sendStatus() {
-    // if (millis() - this->sendStatusMillis > 1000) {
+  void listen() {
+    this->waitMsg();
+    thermometer1.listen();
+    thermometer2.listen();
+    ammeter.listen();
+  }
 
+  void sendStatus() {
     String stats = "AT:" + String(this->AT) + ","
                    + "ST:" + String(this->ST) + ","
                    + "A:" + String(this->A) + ","
@@ -176,9 +168,26 @@ public:
 
     LoRaSerial.println(stats);
     Serial.println(stats);
+  }
 
-    // this->sendStatusMillis = millis();
-    // }
+  // hardware control / logic
+  void setMode(SYSTEM_MODE mode) {
+    this->M = mode;
+  }
+
+  bool modeIs(SYSTEM_MODE mode) {
+    return (this->M == mode);
+  }
+
+  void power(bool on) {
+    if (on) {
+      relay.connect();
+      this->C = 1;
+    } else {
+      relay.cut();
+      this->C = 0;  // set charge to 0
+      this->A = 0;  // pseudo 0 current, prevent throttle
+    }
   }
 
   ~ChargingSystem(void){};

9722a22a9fc260971792d994d48fc53401daad48
t <a@a.a>
2025-07-02 13:31:43 +0800

    feat: update

--- demo_jp/demo_jp.ino
+++ demo_jp/demo_jp.ino
@@ -1,6 +1,7 @@
 #include "SystemEnums.h"
 #include "ChargingSystem.h"
 #include "Utils.h"
+#include <avr/wdt.h>
 
 ChargingSystem chargingSystem;
 Utils utils;
@@ -8,9 +9,11 @@ Utils utils;
 void setup() {
   Serial.begin(9600);
   LoRaSerial.begin(9600);
+  wdt_enable(WDTO_8S);
 }
 
 void loop() {
   chargingSystem.listen();
+  wdt_reset();
   delay(10);
 }

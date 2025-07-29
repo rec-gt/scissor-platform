710f3d6377b13c07f71c914c50000b4928720486
t <a@a.a>
2025-07-29 16:29:38 +0800

    feat: update

--- Machine005v2/DetectSystem.h
+++ Machine005v2/DetectSystem.h
@@ -25,9 +25,9 @@ public:
       return;
     }
 
-    if (status == SYS_RUNNING) {
-      displayOLED.print("", "系統運作中", "0120120120", DISPLAY_SYS_RUNNING);
-    }
+    // if (status == SYS_RUNNING) {
+    //   displayOLED.print("", "系統運作中", "0120120120", DISPLAY_SYS_RUNNING);
+    // }
 
     this->lastStatus = status;
     this->status = status;

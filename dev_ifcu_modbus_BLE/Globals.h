f1c0fe552c14396bc8ff5cdef2988660330f810e
t <a@a.a>
2025-08-13 12:26:49 +0800

    feat: update

---
+++ dev_ifcu_modbus_BLE/Globals.h
@@ -0,0 +1,36 @@
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+/*=== For Modbus ===*/
+RS485Class rs485(Serial2, 2, 3, 4);
+ModbusRTUClientClass mbClient(rs485);
+
+/*=== For NBIoT ===*/
+String serialRes = "";
+String connStr = "";
+String subStr = "";
+String subRecvContent = "";
+String pubMsgContent = "";
+String pubMsgPrepare = "";
+String pubMsgForce = "";
+
+
+/*=== For IFCU ===*/
+enum IFCU_ENUMS {
+  IFCU_ON,
+  IFCU_OFF,
+  IFCU_MODE_AUTO_COOL,
+  IFCU_MODE_MANUAL_COOL,
+  IFCU_MODE_FAN_ONLY,
+  IFCU_MODE_AUTO_HEAT,
+  IFCU_MODE_MANUAL_HEAT,
+  IFCU_FAN_SPEED_LOW,
+  IFCU_FAN_SPEED_MEDIUM,
+  IFCU_FAN_SPEED_HIGH,
+  IFCU_FAN_SPEED_TEN_SPEED,
+  IFCU_ACTION_INCREASE_TEMP,
+  IFCU_ACTION_DECREASE_TEMP,
+};
+
+#endif

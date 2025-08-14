d454dacc24f6216362c6e1b400493283c5967262
t <a@a.a>
2025-08-14 19:40:35 +0800

    feat: update

--- dev_better_display/dev_better_display.ino
+++ dev_better_display/dev_better_display.ino
@@ -1,8 +1,25 @@
+#include "Watchdog.h"
+
 String buffer = "";
+String recv = "";
+
+Watchdog OLEDWatchdog(10000);
+
+byte OLEDResetPin = 30;
 
 void setup() {
   Serial.begin(9600);
   Serial3.begin(9600);
+  pinMode(OLEDResetPin, OUTPUT);
+  digitalWrite(OLEDResetPin, HIGH);
+
+  OLEDWatchdog.enable();
+  OLEDWatchdog.setCallback([]() {
+    digitalWrite(OLEDResetPin, LOW);
+    delay(1000);
+    digitalWrite(OLEDResetPin, HIGH);
+    Serial.println("WATCHDOG RESET OLED");
+  });
 }
 
 void sendToDisplay() {
@@ -16,15 +33,39 @@ void sendToDisplay() {
 }
 
 void overWhelmingDisplay() {
-  String strArr[] = { "STR_STESS_1", "STR_STESS_2", "STR_STESS_3", "STR_STESS_4", "STR_STESS_5", "STR_STESS_6", "STR_STESS_7", "STR_STESS_8", "STR_STESS_9", "STR_STESS_10", "STR_STESS_11" };
-  buffer = strArr[random() % 11];
+  String strArr[] = { "STR_LSAKJJHL_1;STR_LSAKJJHL_2;STR_LSAKJJHL_3", "STR_SAIOPIDUSS_2", "STRAS:ODJ_STESS_3", "STR_STSDAASESS_4", "STRASDSD_STESS_5", "STR_STASDSADESS_6", "STRDGDFS_STESS_7", "STR_STFGFGESS_8", "STRSGDSFG_STESS_9", "STR_STEFGDFGSS_10", "STR_STFDGDFGFDGESS_11", "ASDSFDSSDGDDF", "IOASDOIASJFOIJ", "IUAESFAIUD" };
+  buffer = strArr[random() % 13];
   sendToDisplay();
 }
 
+void listenHeartbeat() {
+  if (Serial3.available() > 0) {
+    while (Serial3.available() > 0) {
+
+      char _byte = Serial3.read();
+
+      if (_byte != '\r' && _byte != '\n') {
+        recv += _byte;
+      }
+
+      if (_byte == '\r') {
+        if (recv == "HB") {
+          OLEDWatchdog.pet();
+          Serial.println("received heartbeat");
+        }
+
+        recv = "";
+      }
+      delay(1);
+    }
+  }
+}
+
 void loop() {
+  OLEDWatchdog.monitor();
   overWhelmingDisplay();
   listenHeartbeat();
-  Serial.println("[STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST]");
-  Serial.flush();
-  delay(10);
+  // Serial.println("[STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST] [STRESS_TEST]");
+  // Serial.flush();
+  delay(100);
 }
--- dev_display_peripheral/dev_display_peripheral.ino
+++ dev_display_peripheral/dev_display_peripheral.ino
@@ -1,4 +1,5 @@
 #include "DisplayOLED.h"
+#define PERIPHERAL_SERIAL Serial3
 
 DisplayOLED displayOLED;
 
@@ -8,47 +9,58 @@ unsigned long prevMillis = 0;
 
 void setup() {
   Serial.begin(9600);
-  Serial3.begin(9600);
+  PERIPHERAL_SERIAL.begin(9600);
   displayOLED.init();
-}
-
-void printMsg() {
-  Serial.println(buffer);
-  const char* charPtr = buffer.c_str();
-  displayOLED.print(charPtr, charPtr, charPtr, random() * 10);
-}
-
-void clearResBuffer() {
-  buffer = "";
 }
 
 void asyncHeartbeat() {
   if (millis() - prevMillis > 1000) {
-    Serial3.println("HB");
-    Serial3.flush();
+    Serial.println("send heartbeat");
+    Serial.flush();
+    PERIPHERAL_SERIAL.println("HB");
+    PERIPHERAL_SERIAL.flush();
     prevMillis = millis();
   }
 }
 
 void listen() {
-  if (Serial3.available() > 0) {
-    while (Serial3.available() > 0) {
+  while (PERIPHERAL_SERIAL.available() > 0) {
 
-      char _byte = Serial3.read();
+    char _byte = PERIPHERAL_SERIAL.read();
 
-      if (_byte != '\r' && _byte != '\n') {
-        buffer += _byte;
-      }
-
-      if (_byte == '\r') {
-        printMsg();
-        clearResBuffer();
-      }
-
-      delay(1);
+    if (_byte != '\r' && _byte != '\n') {
+      buffer += _byte;
     }
-    Serial.println();
+
+    if (_byte == '\r') {
+      const char* charPtr1;
+      const char* charPtr2;
+      const char* charPtr3;
+      Serial.println(buffer);
+
+      int firstDelimiterIndex = buffer.indexOf(';');
+
+      if (firstDelimiterIndex != -1) {
+        String firstPart = buffer.substring(0, firstDelimiterIndex);
+        charPtr1 = firstPart.c_str();
+
+        int secondDelimiterIndex = buffer.indexOf(';', firstDelimiterIndex + 1);
+        if (secondDelimiterIndex != -1) {
+          String secondPart = buffer.substring(firstDelimiterIndex + 1, secondDelimiterIndex);
+          charPtr2 = secondPart.c_str();
+
+          String thirdPart = buffer.substring(secondDelimiterIndex + 1);
+          charPtr3 = thirdPart.c_str();
+        }
+      }
+
+      displayOLED.print(charPtr1, charPtr2, charPtr3);
+      buffer = "";
+    }
+
+    delay(1);
   }
+  Serial.println();
 }
 
 void loop() {

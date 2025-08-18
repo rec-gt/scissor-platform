aed13288823b86c94f8c37275e3dbf8c2f27bb92
t <a@a.a>
2025-08-18 15:20:38 +0800

    feat: update

--- dev_joss_paper_burner/DisplayClient.h
+++ dev_joss_paper_burner/DisplayClient.h
@@ -14,9 +14,26 @@ public:
 
   void prepareBuffer(uint8_t nbiotConn, uint8_t csq, uint8_t dis, uint8_t dos, AnalogInput *ais, AnalogOutput *aos) {
     uint8_t buffer[41];
+
+    buffer[0] = nbiotConn;
+    buffer[1] = csq;
+    buffer[2] = dis;
+    buffer[3] = dos;
+
+    int index = 4;
+    for (int i = 0; i < 12; i++) {
+      buffer[index++] = (ais[i] >> 8) & 0xFF;
+      buffer[index++] = ais[i] & 0xFF;
+    }
+
+    for (int i = 0; i < 4; i++) {
+      buffer[index++] = (aos[i] >> 8) & 0xFF;
+      buffer[index++] = aos[i] & 0xFF;
+    }
   }
 
   void sendBuffer() {
+    DisplaySerial.write(buffer, sizeof(buffer));
   }
 
   ~DisplayClient() {}

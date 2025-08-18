b59700e5437b44bcab04bd7a52465761f0fe8317
t <a@a.a>
2025-08-18 10:19:52 +0800

    feat: update

--- dev_joss_paper_burner/AnalogInput.h
+++ dev_joss_paper_burner/AnalogInput.h
@@ -3,11 +3,16 @@
 class AnalogInput {
 private:
   byte pin;
+  byte mappingMode;
+  uint16_t reading;
+  uint16_t ewma[AI_EWMA_SAMPLE_SIZE];
+  uint16_t readings[AI_EWMA_SAMPLE_SIZE];
+  uint16_t weightedReading;
   uint16_t value;
 
 public:
-  AnalogInput(byte pin)
-    : pin(pin) {
+  AnalogInput(byte pin, byte mappingMode = AI_MAPPING_MODE_4_20MA)
+    : pin(pin), mappingMode(mappingMode) {
     pinMode(pin, INPUT);
   }
 
@@ -18,10 +23,37 @@ public:
       sum += analogRead(this->pin);
     }
 
-    this->value = (sum / OVERSAMPLING_FACTOR) << SHIFT_BITS;
+    this->reading = (sum / OVERSAMPLING_FACTOR) << SHIFT_BITS;
+
+    /*=== Update Reading History ===*/
+    sizt_t lastIdx = AI_EWMA_SAMPLE_SIZE - 1;
+    for (size_t i = 1; i < lastIdx; i++) {
+      this->readings[i - 1] = this->readings[i];
+    }
+    this->readings[lastIdx] = this->reading;
+  }
+
+  unsigned int getReading() {
+    return this->reading;
+  }
+
+  unsigned int getWeightedReading() {
+    for (sizt_t i = 1; i < AI_EWMA_SAMPLE_SIZE; i++) {
+      this->ewma[i] = AI_EWMA_ALPHA * this->readings[i] + (1 - AI_EWMA_ALPHA) * this->ewma[i - 1];
+    }
   }
 
   unsigned int getValue() {
+    switch (this->mappingMode) {
+      case AI_MAPPING_MODE_4_20MA:
+        this->value = map(this->reading, 0, 16368, 0, 50000);
+        break;
+      case AI_MAPPING_MODE_0_10V:
+        this->value = map(this->reading, 3360, 16368, 10000, 50000);
+        break;
+      default:
+        this->value = 0;
+    }
     return this->value;
   }
 };
--- dev_joss_paper_burner/Globals.h
+++ dev_joss_paper_burner/Globals.h
@@ -4,6 +4,10 @@
 #define SHIFT_BITS 4
 #define OVERSAMPLING_FACTOR 256  // 2 ^ (2 * 4)
 
+#define AI_MAPPING_MODE_4_20MA 0
+#define AI_MAPPING_MODE_0_10V 1
+#define AI_EWMA_SAMPLE_SIZE 5
+#define AI_EWMA_ALPHA 0.2
 // 經驗數值
 // Serial.println(map(analogInputs[i].getValue(), 0, 16368, 0, 50000));// for 0-5V
 // Serial.println(map(analogInputs[i].getValue(), 3360, 16368, 10000, 50000)); // for 4-20mA

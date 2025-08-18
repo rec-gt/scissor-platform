061b27bc2b18449418694423c2156223f8aa6ed4
t <a@a.a>
2025-08-18 17:43:18 +0800

    feat: update

--- dev_display_server_for_nbiot/DisplayOLED.h
+++ dev_display_server_for_nbiot/DisplayOLED.h
@@ -32,6 +32,7 @@ public:
       u8g2.drawStr(0, 7, "RGT NBIoT Controller v2.0");
       u8g2.drawLine(0, 9, 128, 9);
 
+      /*=== DI ===*/
       u8g2.drawStr(0, 18, "Di");
       u8g2.drawStr(12, 18, "0");
       u8g2.drawStr(17, 18, "0");
@@ -41,16 +42,36 @@ public:
       u8g2.drawStr(37, 18, "0");
       u8g2.drawStr(42, 18, "0");
       u8g2.drawStr(47, 18, "0");
+
+      /*=== DO ===*/
       u8g2.drawStr(72, 18, "Do");
-      u8g2.drawStr(84, 18, "11111111");
+      u8g2.drawStr(84, 18, "0");
+      u8g2.drawStr(17, 18, "0");
+      u8g2.drawStr(22, 18, "0");
+      u8g2.drawStr(27, 18, "0");
+      u8g2.drawStr(32, 18, "0");
+      u8g2.drawStr(37, 18, "0");
+      u8g2.drawStr(42, 18, "0");
+      u8g2.drawStr(47, 18, "0");
       u8g2.drawLine(0, 20, 128, 20);
 
+      /*=== AI ===*/
       u8g2.drawStr(0, 29, "Ai");
-      u8g2.drawStr(12, 29, "99999 99999 99999 99999");
-      u8g2.drawStr(12, 37, "99999 99999 99999 99999");
-      u8g2.drawStr(12, 45, "99999 99999 99999 99999");
+      u8g2.drawStr(12, 29, "99999");
+      u8g2.drawStr(42, 29, "99999");
+      u8g2.drawStr(72, 29, "99999");
+      u8g2.drawStr(102, 29, "99999");
+      u8g2.drawStr(12, 37, "99999");
+      u8g2.drawStr(42, 37, "99999");
+      u8g2.drawStr(72, 37, "99999");
+      u8g2.drawStr(102, 37, "99999");
+      u8g2.drawStr(12, 45, "99999");
+      u8g2.drawStr(42, 45, "99999");
+      u8g2.drawStr(72, 45, "99999");
+      u8g2.drawStr(102, 45, "99999");
       u8g2.drawLine(0, 47, 128, 47);
 
+      /*=== AO ===*/
       u8g2.drawStr(0, 56, "Ao");
       u8g2.drawStr(12, 56, "99999 99999");
       u8g2.drawStr(12, 64, "99999 99999");

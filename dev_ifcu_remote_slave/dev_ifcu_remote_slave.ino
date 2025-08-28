1e331531fbb1b053ac15c0be2947faca1df176ed
t <a@a.a>
2025-08-28 17:28:08 +0800

    feat: update

--- dev_ifcu_remote_slave/dev_ifcu_remote_slave.ino
+++ dev_ifcu_remote_slave/dev_ifcu_remote_slave.ino
@@ -9,7 +9,11 @@ const char* password = "guest@@2022";
 const char* serverName = "http://10.236.208.127:3010";
 String url = String(serverName) + "/f-l/1";
 
+// TODO: ASK FOR STATIC IP
+
 WebServer server(80);
+
+unsigned long prevMillis = millis();
 
 void handleGetData() {
   server.send(200, "application/json", "2500,2500,2,2,1700,3000");
@@ -33,23 +37,29 @@ void setup() {
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin(url);
-    int httpResponseCode = http.GET();
+  if (millis() - prevMillis > 3000) {
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+      http.begin(url);
+      int httpResponseCode = http.GET();
 
-    if (httpResponseCode > 0) {
-      Serial.printf("HTTP Response code: %d\n", httpResponseCode);
-      String response = http.getString();
-      Serial.println(response);
+      if (httpResponseCode > 0) {
+        Serial.printf("HTTP Response code: %d\n", httpResponseCode);
+        String response = http.getString();
+        Serial.println(response);
+      } else {
+        Serial.printf("Error code: %d\n", httpResponseCode);
+      }
+
+      http.end();
+
     } else {
-      Serial.printf("Error code: %d\n", httpResponseCode);
+      Serial.println("WiFi Disconnected");
     }
-    http.end();
 
-  } else {
-    Serial.println("WiFi Disconnected");
+    prevMillis = millis();
   }
+
 
   server.handleClient();
 

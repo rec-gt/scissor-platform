9475b17c1bf53ae29662b0a9073839d1adf998e6
t <a@a.a>
2025-08-29 17:12:49 +0800

    feat: update

--- dev_ifcu_remote_slave/SerialSend.h
+++ dev_ifcu_remote_slave/SerialSend.h
@@ -20,32 +20,13 @@ private:
     delay(delayTime);
   }
 
-  void capturePayload() {
-    int idx = this->payload.indexOf("PAYLOAD:");
-    if (idx > -1) {
-      this->payload = this->payload.substring(8, 100);
-    }
-  }
 public:
-  String payload = "2500,2500,2,2,1700,3000";
+  String payload = "2500,2,2";
 
   SerialSend(){};
 
-  void listen() {
-    while (Serial2.available() > 0) {
-      char c = Serial2.read();
-
-      Serial.print(c);
-
-      if (c != '\r' && c != '\n') {
-        this->serialSend += c;
-      }
-
-      if (c == '\r') {
-        this->capturePayload();
-        this->clearResBuffer();
-      }
-    }
+  void send() {
+    this->printlnFlush(payload);
   }
 
   ~SerialSend(){};

fd1379ede5fd228da4e1a8fa3194d8265ef1e907
t <a@a.a>
2025-09-01 11:32:33 +0800

    feat: update

--- dev_ifcu_remote_broker/Globals.h
+++ dev_ifcu_remote_broker/Globals.h
@@ -5,14 +5,14 @@
 #define REQUEST_VALUES_LEN 4
 #define RESPONSE_VALUES_LEN 7
 
-int requestValues[REQUEST_VALUES_LEN] = {
+uint16_t requestValues[REQUEST_VALUES_LEN] = {
   0,  // on/off
   0,  // set point temp
   0,  // mode
   0,  // speed
 };
 
-int responseValues[RESPONSE_VALUES_LEN] = {
+uint16_t responseValues[RESPONSE_VALUES_LEN] = {
   0,  // on/off
   0,  // room temp
   0,  // set point temp
--- dev_ifcu_remote_broker/SerialBroker.h
+++ dev_ifcu_remote_broker/SerialBroker.h
@@ -5,6 +5,11 @@
 
 class SerialBroker {
 private:
+  bool isReceiving = false;
+  uint16_t idx = 0;
+
+  uint8_t serialRecvBuffer[8] = { 0x5B, 1, (2500 & 0xFF), ((2500 >> 8) & 0xFF), 2, 2, 210, 0x5D };
+
   String serialRecv = "PAYLOAD:1,2500,2,2";
   String serialSend = "PAYLOAD:1,2500,2500,2,2,1700,3000";
 
@@ -22,20 +27,14 @@ private:
     delay(delayTime);
   }
 
-  void captureRecvPayload() {
-    int idx = this->serialRecv.indexOf("PAYLOAD:");
-    if (idx > -1) {
-      this->serialRecvPayload = this->serialRecv.substring(8, 100);
-
-      this->strToArr(this->serialRecvPayload, requestValues, REQUEST_VALUES_LEN);
-
-      Serial.println("===================");
-
-      for (int i = 0; i < 4; i++) {
-        Serial.print(requestValues[i]);
-        Serial.print(", ");
-      }
+  uint8_t getChecksum(uint8_t *buffer, uint8_t idx_from, uint8_t idx_to) {
+    uint8_t checksum = 0;
+    for (uint8_t i = idx_from; i <= idx_to; i++) {
+      Serial.print(buffer[i]);
+      Serial.print("->");
+      checksum += buffer[i];
     }
+    return checksum;
   }
 
   void strToArr(String input, int *target, int size) {
@@ -56,13 +55,41 @@ private:
     target[index] = temp.toInt();
   }
 
+  void captureRecvPayload() {
+
+    requestValues[0] = this->serialRecvBuffer[1];
+    requestValues[1] = (this->serialRecvBuffer[2] << 8) | this->serialRecvBuffer[3];
+    requestValues[2] = this->serialRecvBuffer[4];
+    requestValues[3] = this->serialRecvBuffer[5];
+
+    // int idx = this->serialRecv.indexOf("PAYLOAD:");
+    // if (idx > -1) {
+    //   this->serialRecvPayload = this->serialRecv.substring(8, 100);
+
+    //   this->strToArr(this->serialRecvPayload, requestValues, REQUEST_VALUES_LEN);
+
+    //   Serial.print("\r\n===================\r\n");
+    //   Serial.println(this->serialRecv);
+    //   Serial.println(this->serialRecvPayload);
+
+    //   for (int i = 0; i < 4; i++) {
+    //     Serial.print(requestValues[i]);
+    //     Serial.print(", ");
+    //   }
+    // }
+  }
+
+
 public:
   String serialRecvPayload = "";
   String serialSendPayload = "";
 
 
 
-  SerialBroker(){};
+  SerialBroker() {
+    Serial.print("\r\n===================\r\n");
+    Serial.print(this->getChecksum(this->serialRecvBuffer, 1, 5));
+  };
 
   void listen() {
     this->captureRecvPayload();
@@ -80,6 +107,32 @@ public:
       if (c == '\r') {
         this->captureRecvPayload();
         this->clearSerialRecv();
+      }
+    }
+  }
+
+  void listenByte() {
+    while (Serial1.available() > 0) {
+      uint8_t rb = Serial1.read();
+
+      if (rb == 0x5B) {
+        this->isReceiving = true;
+        this->idx = 0;
+      }
+
+      if (this->isReceiving) {
+        this->serialRecvBuffer[idx++] = rb;
+
+        if (rb == 0x5D) {
+          this->isReceiving = false;
+          this->idx = 0;
+
+          uint8_t payloadChecksum = this->serialRecvBuffer[6];
+          uint8_t calculatedChecksum = this->getChecksum(this->serialRecvBuffer, 1, 5);
+          if (payloadChecksum == calculatedChecksum) {
+            this->captureRecvPayload();
+          }
+        }
       }
     }
   }

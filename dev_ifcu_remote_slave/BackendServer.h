18c105d2da68b18006639f11bd3cadd750805516
t <a@a.a>
2025-09-02 16:47:33 +0800

    feat: update

--- dev_ifcu_remote_slave/BackendServer.h
+++ dev_ifcu_remote_slave/BackendServer.h
@@ -11,11 +11,22 @@ public:
 
   void init() {
     server.on("/get", HTTP_GET, []() {
-      
+      server.send(200, "text/plain", "123");
     });
-    server.on("/set", HTTP_GET, []() {});
-    server.onNotFound(handleNotFound);
+
+    server.on("/set", HTTP_GET, []() {
+      if (server.hasArg("value")) {
+        String valStr = server.arg("value");
+        currentValue = valStr.toInt();
+        server.send(200, "text/plain", "Value set to: " + String(currentValue));
+        Serial.println("GET /set - Set to: " + String(currentValue));
+      }
+    });
   };
+
+  void loop() {
+    server.handleClient();
+  }
 
   ~BackendServer(){};
 };
--- dev_ifcu_remote_slave/dev_ifcu_remote_slave.ino
+++ dev_ifcu_remote_slave/dev_ifcu_remote_slave.ino
@@ -1,9 +1,11 @@
 #include "Globals.h"
 #include "SerialBroker.h"
 #include "WiFiBroker.h"
+#include "BackendServer.h"
 
 SerialBroker serialBroker;
 WiFiBroker wifiBroker;
+BackendServer backendServer;
 
 void setup() {
   Serial.begin(9600);
@@ -11,10 +13,12 @@ void setup() {
   Serial2.begin(9600, SERIAL_8N1, 16, 17);  // send request to modbus-broker
 
   wifiBroker.init();
+  backendServer.init();
 }
 
 void loop() {
   serialBroker.loop();
   wifiBroker.loop();
+  backendServer.loop();
   delay(10);
 }

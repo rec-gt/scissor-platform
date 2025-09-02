a65a23d1d6fa97430e9a6b590be7414e1bbee869
t <a@a.a>
2025-09-02 11:06:09 +0800

    feat: update

--- dev_ifcu_remote_slave/WiFiBroker.h
+++ dev_ifcu_remote_slave/WiFiBroker.h
@@ -7,10 +7,10 @@
 
 const char* ssid = "iFCU-16f-test-ckyt";
 const char* password = "iFCU-16f-test-ckyt";
+const char* masterIP = "192.168.1.100";
+const int masterPort = 80;
 
-WiFiServer server(80);
-
-AutoTimer timer;
+WiFiClient client;
 
 class WiFiBroker {
 private:
@@ -18,15 +18,43 @@ public:
   WiFiBroker(){};
 
   void init() {
-    WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
-
     while (WiFi.status() != WL_CONNECTED) {
       delay(1000);
       Serial.println("Connecting to WiFi...");
     }
-
+    Serial.println("Connected to WiFi");
+    Serial.print("Slave IP: ");
     Serial.println(WiFi.localIP());
+  }
+
+  void loop() {
+    this->handleReconnect();
+    this->sendDataToMaster();
+  }
+
+  void handleReconnect() {
+    if (!client.connected()) {
+      Serial.println("Connecting to master...");
+      if (client.connect(masterIP, masterPort)) {
+        Serial.println("Connected to master");
+      } else {
+        Serial.println("Connection failed. Retrying in 5s...");
+        delay(5000);
+        return;
+      }
+    }
+  }
+
+  void sendDataToMaster() {
+    String data = "Hello from slave: " + String(millis());
+    client.println(data);
+
+    if (client.available()) {
+      String response = client.readStringUntil('\n');
+      response.trim();
+      Serial.println("Received from master: " + response);
+    }
   }
 
   ~WiFiBroker(){};
--- dev_ifcu_remote_slave/dev_ifcu_remote_slave.ino
+++ dev_ifcu_remote_slave/dev_ifcu_remote_slave.ino
@@ -11,49 +11,10 @@ void setup() {
   Serial2.begin(9600, SERIAL_8N1, 16, 17);  // send request to modbus-broker
 
   wifiBroker.init();
-  
-  // WiFi.mode(WIFI_STA);
-  // WiFi.begin(ssid, password);
-
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(500);
-  //   Serial.print(".");
-  // }
-
-  // server.on("/get", []() {
-  //   server.send(200, "application/json", serialRecv.payload);
-  // });
-  // server.begin();
-
-  // Serial.println("IP Address: " + WiFi.localIP().toString());
-  // Serial.println("HTTP server started!");
 }
 
 void loop() {
-  // if (millis() - prevMillis > 3000) {
-  //   if (WiFi.status() == WL_CONNECTED) {
-  //     HTTPClient http;
-  //     http.begin(url);
-  //     int httpResponseCode = http.GET();
-
-  //     if (httpResponseCode > 0) {
-  //       Serial.printf("HTTP Response code: %d\n", httpResponseCode);
-  //       String response = http.getString();
-  //       Serial.println(response);
-  //     } else {
-  //       Serial.printf("Error code: %d\n", httpResponseCode);
-  //     }
-
-  //     http.end();
-
-  //   } else {
-  //     Serial.println("WiFi Disconnected");
-  //   }
-
-  //   prevMillis = millis();
-  // }
-
   serialBroker.loop();
-
+  wifiBroker.loop();
   delay(10);
 }

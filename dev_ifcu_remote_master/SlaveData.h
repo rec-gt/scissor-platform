eec1b8b35e9e12e2dc411a435ef9fe96f31b590e
t <a@a.a>
2025-09-02 12:00:06 +0800

    feat: update

--- dev_ifcu_remote_master/SlaveData.h
+++ dev_ifcu_remote_master/SlaveData.h
@@ -1,7 +1,7 @@
 class SlaveData {
 public:
   uint8_t id;
-  char* name = "iFCU-16";
+  String name = "iFCU-16";
   bool onOff = false;
   uint8_t mode = 0;
   uint8_t speed = 0;
@@ -9,7 +9,49 @@ public:
   uint16_t setTemp = 2500;
   uint16_t lowerTempLimit = 1500;
   uint16_t higherTempLimit = 2700;
+  String dataStr = "";
 
-  SlaveData() {}
+  SlaveData() {
+    void set(
+      uint8_t id,
+      String name,
+      bool onOff,
+      uint8_t mode,
+      uint8_t speed,
+      uint16_t roomTemp,
+      uint16_t setTemp,
+      uint16_t lowerTempLimit,
+      uint16_t higherTempLimit) {
+      this->id = id;
+      this->name = name;
+      this->onOff = onOff;
+      this->mode = mode;
+      this->speed = speed;
+      this->roomTemp = roomTemp;
+      this->setTemp = setTemp;
+      this->lowerTempLimit = lowerTempLimit;
+      this->higherTempLimit = higherTempLimit;
+    };
+
+    void get() {
+      dataStr += String(id);
+      dataStr += String(",");
+      dataStr += String(name);
+      dataStr += String(",");
+      dataStr += String(onOff);
+      dataStr += String(",");
+      dataStr += String(mode);
+      dataStr += String(",");
+      dataStr += String(speed);
+      dataStr += String(",");
+      dataStr += String(roomTemp);
+      dataStr += String(",");
+      dataStr += String(setTemp);
+      dataStr += String(",");
+      dataStr += String(lowerTempLimit);
+      dataStr += String(",");
+      dataStr += String(higherTempLimit);
+    };
+  }
   ~SlaveData() {}
 };

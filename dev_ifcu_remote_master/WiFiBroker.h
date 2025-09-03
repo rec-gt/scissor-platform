5d66a9a7b28c06d914269bd129298e573c6089ea
t <a@a.a>
2025-09-03 09:56:08 +0800

    feat: update

--- dev_ifcu_remote_master/WiFiBroker.h
+++ dev_ifcu_remote_master/WiFiBroker.h
@@ -3,9 +3,6 @@
 #ifndef WiFiBroker_H
 #define WiFiBroker_H
 
-#define MAX_SLAVES 5
-#define SERVER_PORT 80
-
 const char* apSSID = "ifcu-remote-16f-test";
 const char* apPassword = "ifcu-remote-16f-test";
 
@@ -13,8 +10,7 @@ IPAddress staticIP(192, 168, 1, 100);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-WiFiServer server(SERVER_PORT);
-WiFiClient slaveClients[MAX_SLAVES];
+WiFiServer server(80);
 
 class WiFiBroker {
 private:
@@ -22,34 +18,15 @@ public:
   WiFiBroker(){};
 
   void init() {
-    WiFi.mode(WIFI_AP);
-    if (!WiFi.softAPConfig(staticIP, gateway, subnet)) {
-      Serial.println("Failed to configure AP static IP");
-    }
-
-    WiFi.softAP(apSSID, apPassword);
-    Serial.println("Access Point started");
-    Serial.print("Master AP IP: ");
-    Serial.println(WiFi.softAPIP());
-
-    server.begin();
-    Serial.println("TCP server started");
+   
   }
 
   void loop() {
-    this->handleNewConnection();
+    this->handleNewClientJoin();
   }
 
-  void handleNewConnection() {
-    WiFiClient client = server.available();
-    if (client) {
-      Serial.println("New client connected");
-
-      Serial.print("Client IP address: ");
-      Serial.println(client.remoteIP());
-
-      client.stop();
-    }
+  void handleNewClientJoin() {
+   
   }
 
   ~WiFiBroker(){};
--- dev_ifcu_remote_master/dev_ifcu_remote_master.ino
+++ dev_ifcu_remote_master/dev_ifcu_remote_master.ino
@@ -1,13 +1,40 @@
 #include <WiFi.h>
-#include "WiFiBroker.h"
 
-WiFiBroker wifiBroker;
+const char* apSSID = "ifcu-remote-16f-test";
+const char* apPassword = "ifcu-remote-16f-test";
+
+IPAddress staticIP(192, 168, 1, 100);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+WiFiServer server(80);
 
 void setup() {
   Serial.begin(9600);
-  wifiBroker.init();
+
+  WiFi.mode(WIFI_AP);
+  if (!WiFi.softAPConfig(staticIP, gateway, subnet)) {
+    Serial.println("Failed to configure AP static IP");
+  }
+
+  WiFi.softAP(apSSID, apPassword);
+  Serial.println("Access Point started");
+  Serial.print("Master AP IP: ");
+  Serial.println(WiFi.softAPIP());
+
+  server.begin();
+  Serial.println("TCP server started");
 }
 
 void loop() {
-  wifiBroker.loop();
+  WiFiClient newClient = server.available();
+
+  if (newClient) {
+    Serial.println("New slave trying to connect...");
+    Serial.print(" - IP: ");
+    Serial.print(newClient.remoteIP());
+    Serial.print(", Port: ");
+    Serial.println(newClient.remotePort());
+  }
+  delay(100);
 }

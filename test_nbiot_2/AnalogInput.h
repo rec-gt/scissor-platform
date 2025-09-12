4cf5bcf7b092d6b06176645443d4f15eeffe5e7c
t <a@a.a>
2025-09-12 14:33:57 +0800

    feat: update

--- test_nbiot_2/AnalogInput.h
+++ test_nbiot_2/AnalogInput.h
@@ -75,9 +75,7 @@ public:
         } else if (this->smoothedReading <= this->bp5) {
           this->value = map(this->smoothedReading, this->bp4 + 1, this->bp5, 16001, 20000);
         }
-
-        Serial.print("14-bit Raw: ");
-        Serial.println(this->value);
+        this->value = map(this->smoothedReading, 0, 20000, 0, 4095);
         break;
       case AI_MAPPING_MODE_0_10V:
         if (this->smoothedReading <= this->bp1) {
@@ -91,13 +89,13 @@ public:
         } else if (this->smoothedReading <= this->bp5) {
           this->value = map(this->smoothedReading, this->bp4 + 1, this->bp5, 5001, 10000);
         }
-
-        Serial.print("14-bit Raw: ");
-        Serial.println(this->value);
+        this->value = map(this->smoothedReading, 0, 10000, 0, 4095);
         break;
       default:
         this->value = 0;
     }
+    Serial.print("14-bit Raw: ");
+    Serial.println(this->value);
     return this->value;
   }
 

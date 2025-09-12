c822af7883f45e7b66261b7e79aa1f5c90b1459a
t <a@a.a>
2025-09-12 13:40:05 +0800

    feat: update

--- test_nbiot_2/AnalogInput.h
+++ test_nbiot_2/AnalogInput.h
@@ -17,11 +17,21 @@ public:
   uint16_t smoothedReading;
   uint16_t readings[AI_SMOOTHING_SAMPLE_SIZE];
   uint16_t value;
+  uint16_t bp1;
+  uint16_t bp2;
+  uint16_t bp3;
+  uint16_t bp4;
+  uint16_t bp5;
 
   AnalogInput() {}
 
   AnalogInput(byte pin, byte mappingMode = AI_MAPPING_MODE_4_20MA)
     : pin(pin), mappingMode(mappingMode) {
+    pinMode(pin, INPUT);
+  }
+
+  AnalogInput(byte pin, byte mappingMode, int bp1, int bp2, int bp3, int bp4, int bp5)
+    : pin(pin), mappingMode(mappingMode), bp1(bp1), bp2(bp2), bp3(bp3), bp4(bp4), bp5(bp5) {
     pinMode(pin, INPUT);
   }
 
@@ -54,14 +64,22 @@ public:
   uint16_t getValue(bool w = false) {  // turn smoothings on or off
     switch (this->mappingMode) {
       case AI_MAPPING_MODE_4_20MA:
-        Serial.print("14-bit Raw: ");
-        Serial.println(this->smoothedReading);
 
-        if (this->smoothedReading <= 1593) {
-          this->value = map(this->smoothedReading, 0, 1593, 0, 195);
-        } else {
-          this->value = map(this->smoothedReading, 1594, 8067, 196, 4096);
+
+        if (this->smoothedReading <= this->bp1) {
+          this->value = map(this->smoothedReading, 0, this->bp1, 0, 4000);
+        } else if (this->smoothedReading <= bp2) {
+          this->value = map(this->smoothedReading, this->bp1 + 1, this->bp2, 4001, 8000);
+        } else if (this->smoothedReading <= bp3) {
+          this->value = map(this->smoothedReading, this->bp2 + 1, this->bp3, 8001, 12000);
+        } else if (this->smoothedReading <= bp4) {
+          this->value = map(this->smoothedReading, this->bp3 + 1, this->bp4, 12001, 16000);
+        } else if (this->smoothedReading <= bp5) {
+          this->value = map(this->smoothedReading, this->bp4 + 1, this->bp5, 16001, 20000);
         }
+
+        Serial.print("14-bit Raw: ");
+        Serial.println(this->value);
         break;
       case AI_MAPPING_MODE_0_10V:
         if (this->smoothedReading <= 3242) {

eef0b28a9817066add09adbe53880512ca0c70c0
t <a@a.a>
2025-09-19 09:41:34 +0800

    feat: update

--- test_nbiot_2/test_nbiot_2.ino
+++ test_nbiot_2/test_nbiot_2.ino
@@ -34,6 +34,12 @@
 #define AO_PIN_3 6
 #define AO_PIN_4 7
 
+// TODO: change to these pins later
+// #define AO_PIN_1 2
+// #define AO_PIN_2 3
+// #define AO_PIN_3 5
+// #define AO_PIN_4 6
+
 #define DRY_CONTACT_PIN_1 27
 #define DRY_CONTACT_PIN_2 28
 #define DRY_CONTACT_PIN_3 29
@@ -176,7 +182,10 @@ void loop() {
 
   /*=== Register MainSystem ===*/
   mainSystem.loop();
-
+  // analogOutputs[0].set(255);
+  // analogOutputs[1].set(255);
+  // analogOutputs[2].set(255);
+  // analogOutputs[3].set(255);
   /*=== Register Outputs Testing ===*/
   // test.DO(digitalOutputs);
   // test.AO(analogOutputs);

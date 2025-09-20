3d39c884764e2c2a7c29d2e92b21757ceda6dfe0
t <a@a.a>
2025-09-20 09:52:47 +0800

    feat: update

--- test_nbiot_2/Modbus485.h
+++ test_nbiot_2/Modbus485.h
@@ -35,7 +35,9 @@ public:
     int idx = rs485SerialRecv.indexOf(cmpStr);
     if (idx > -1) {
       digitalWrite(RE_DE_PIN, HIGH);
-      Serial3.println("[Hello from RGT Hello from RGT Hello from RGT]");
+      Serial3.println(F("
+      Setting\r\n
+      1. Analog Input Mode\r\n"));
       Serial3.flush();
     }
   }

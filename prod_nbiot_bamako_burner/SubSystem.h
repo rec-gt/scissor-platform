4bb02d6dba16a90924e7a7126e74123c9997337a
t <a@a.a>
2025-10-08 15:58:38 +0800

    feat: update

--- prod_nbiot_bamako_burner/SubSystem.h
+++ prod_nbiot_bamako_burner/SubSystem.h
@@ -22,7 +22,9 @@ public:
     : digitalInputs(digitalInputs), digitalOutputs(digitalOutputs), analogInputs(analogInputs), analogOutputs(analogOutputs) {}
 
   void loop() {
-    Serial.println(analogInputs[0].getValue());
+    int actualTemp = map(analogInputs[0].getValue(), 0, 511, 0, 1300);
+    int displayTemp = map(actualTemp, 0, 1300, 0, 255);
+    analogOutputs[0].set(displayTemp);
   }
 
   ~SubSystem() {}

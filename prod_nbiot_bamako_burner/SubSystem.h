8517fcf92560c257a18ec4a52811a019443580a6
t <a@a.a>
2025-10-10 09:49:57 +0800

    feat: update

--- prod_nbiot_bamako_burner/SubSystem.h
+++ prod_nbiot_bamako_burner/SubSystem.h
@@ -18,6 +18,8 @@ private:
   AnalogOutput *analogOutputs;
 
 public:
+  int configurableTemp = 450;
+
   SubSystem(DigitalInput *digitalInputs, DigitalOutput *digitalOutputs, AnalogInput *analogInputs, AnalogOutput *analogOutputs)
     : digitalInputs(digitalInputs), digitalOutputs(digitalOutputs), analogInputs(analogInputs), analogOutputs(analogOutputs) {}
 
@@ -30,6 +32,46 @@ public:
       int actualTemp = map(reading, 99, 504, 0, 1300);
       int displayTemp = map(actualTemp, 0, 1300, 0, 255);
       analogOutputs[0].set(displayTemp);
+    }
+  }
+
+  int readingToActualTemp(int reading) {
+    if (reading < 99) {
+      return 0;
+    } else {
+      return map(reading, 99, 504, 0, 1300);
+    }
+  }
+
+  void handle800Temp() {
+    int reading = analogInputs[0].getValue();
+    int actualTemp = readingToActualTemp(reading);
+    int aoValue = map(actualTemp, 0, 1300, 0, 255);
+
+    // === display actual temperature ===
+    analogOutputs[0].set(aoValue);
+
+    // === logic control ===
+    if (actualTemp > 800) {
+      digitalOutputs[0].cut();
+    } else {
+      digitalOutputs[0].connect();
+    }
+  }
+
+  void handleConfigurableTemp() {
+    int reading = analogInputs[1].getValue();
+    int actualTemp = readingToActualTemp(reading);
+    int aoValue = map(actualTemp, 0, 1300, 0, 255);
+
+    // === display actual temperature ===
+    analogOutputs[1].set(aoValue);
+
+    // === logic control ===
+    if (actualTemp > this->configurableTemp) {
+      digitalOutputs[1].cut();
+    } else {
+      digitalOutputs[1].connect();
     }
   }
 
--- prod_nbiot_bamako_burner/prod_nbiot_bamako_burner.ino
+++ prod_nbiot_bamako_burner/prod_nbiot_bamako_burner.ino
@@ -136,7 +136,7 @@ void setup() {
   nbiotSerialRecv.reserve(128);
   nbiotPubMsgPayload.reserve(256);
   nbiotPubMsgCommand.reserve(256);
-  bool res = rubbishStr.reserve(278);  //push it to limit
+  bool res = rubbishStr.reserve(278);  // push it to limit
 
   Serial.print(res ? F("[Str Space OK]") : F("[String Space NOT OK]"));
 

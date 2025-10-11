37af3b2e22a99e70d460c339a628d81446332157
t <a@a.a>
2025-10-11 12:49:02 +0800

    feat: update

--- test_nbiot_2/AnalogInput.h
+++ test_nbiot_2/AnalogInput.h
@@ -3,8 +3,6 @@
 
 #define AI_SHIFT_BITS 4
 #define AI_OVERSAMPLING_FACTOR 256  // 2 ^ (2 * 4)
-#define AI_MAPPING_MODE_4_20MA 0
-#define AI_MAPPING_MODE_0_10V 1
 #define AI_SMOOTHING_SAMPLE_SIZE 24
 
 class AnalogInput {
@@ -14,7 +12,7 @@ protected:
 public:
   uint16_t reading;
   uint16_t smoothedReading;
-  uint16_t readinHistory[AI_SMOOTHING_SAMPLE_SIZE];
+  uint16_t readingHistory[AI_SMOOTHING_SAMPLE_SIZE];
   uint16_t value;
 
   AnalogInput() {}
@@ -37,14 +35,14 @@ public:
 
     /*=== Update Reading History ===*/
     for (size_t i = 1; i < AI_SMOOTHING_SAMPLE_SIZE; i++) {
-      this->readinHistory[i - 1] = this->readinHistory[i];
+      this->readingHistory[i - 1] = this->readingHistory[i];
     }
-    this->readinHistory[AI_SMOOTHING_SAMPLE_SIZE - 1] = this->reading;
+    this->readingHistory[AI_SMOOTHING_SAMPLE_SIZE - 1] = this->reading;
 
     /*=== get smoothed reading ===*/
     uint32_t smoothSum = 0;
     for (size_t i = 0; i < AI_SMOOTHING_SAMPLE_SIZE; i++) {
-      smoothSum += this->readinHistory[i];
+      smoothSum += this->readingHistory[i];
     }
     this->smoothedReading = smoothSum / AI_SMOOTHING_SAMPLE_SIZE;
   }
@@ -56,16 +54,47 @@ public:
 };
 
 class AnalogInputFaster : public AnalogInput {
+private:
+  uint16_t findMostFrequentValue(uint16_t arr[], int size) {
+    uint16_t mostFrequent = 0;
+    int maxCount = 0;
+
+    for (int i = 0; i < size; i++) {
+      int count = 0;
+
+      for (int j = 0; j < size; j++) {
+        if (arr[i] == arr[j]) {
+          count++;
+        }
+      }
+
+      if (count > maxCount) {
+        maxCount = count;
+        mostFrequent = arr[i];
+      }
+    }
+
+    return mostFrequent;
+  }
+
 public:
   AnalogInputFaster(byte pin)
     : AnalogInput(pin) {}
 
   void listen() {
     uint32_t avg = 0;
-    for (size_t i = 0; i < 64; i++) {
+    for (size_t i = 0; i < 32; i++) {
       avg += analogRead(this->pin);
     };
-    this->value = (avg / 64);
+    this->reading = avg / 32;
+
+    /*=== Update Reading History ===*/
+    for (size_t i = 1; i < AI_SMOOTHING_SAMPLE_SIZE; i++) {
+      this->readingHistory[i - 1] = this->readingHistory[i];
+    }
+    this->readingHistory[AI_SMOOTHING_SAMPLE_SIZE - 1] = this->reading;
+
+    this->value = this->findMostFrequentValue(this->readingHistory, AI_SMOOTHING_SAMPLE_SIZE);
   }
 
   uint16_t getValue() {

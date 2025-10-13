868734e17eb2a7cd8df82d39ae8ac34c8f9e0f8b
t <a@a.a>
2025-10-13 17:13:37 +0800

    feat: update

--- prod_nbiot_bamako_burner/SubSystem.h
+++ prod_nbiot_bamako_burner/SubSystem.h
@@ -105,12 +105,12 @@ public:
     temperatureSensor1.breakpoint(relay1, 60);
 
     temperatureSensor2.listen();
-    temperatureSensor1.displayTemp(1);
-    temperatureSensor1.breakpoint(relay2, 60);
+    temperatureSensor2.displayTemp(1);
+    temperatureSensor2.breakpoint(relay2, 60);
 
     temperatureSensor3.listen();
-    temperatureSensor1.displayTemp(2);
-    temperatureSensor1.breakpoint(relay3, 60);
+    temperatureSensor3.displayTemp(2);
+    temperatureSensor3.breakpoint(relay3, 60);
   }
 
   ~SubSystem() {}

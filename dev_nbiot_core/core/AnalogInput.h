70dc095cead77327bf89430f9d324e970bc4dbea
t <a@a.a>
2025-10-21 10:42:45 +0800

    feat: update

--- dev_nbiot_core/core/AnalogInput.h
+++ dev_nbiot_core/core/AnalogInput.h
@@ -33,32 +33,45 @@ public:
   }
 
   void listen() {
-    uint32_t sum = 0;
+    if (this->resolution == AI_RESOLUTION_4096) {
+      uint32_t sum = 0;
 
-    for (int i = 0; i < AI_OVERSAMPLING_FACTOR; i++) {
-      sum += analogRead(this->pin);
-      delayMicroseconds(1);
+      for (int i = 0; i < AI_OVERSAMPLING_FACTOR; i++) {
+        sum += analogRead(this->pin);
+        delayMicroseconds(1);
+      }
+
+      /*=== Update Reading ===*/
+      this->reading = sum >> AI_SHIFT_BITS;
+
+      /*=== Update Reading History ===*/
+      for (size_t i = 1; i < AI_SMOOTHING_SAMPLE_SIZE; i++) {
+        this->readinHistory[i - 1] = this->readinHistory[i];
+      }
+      this->readinHistory[AI_SMOOTHING_SAMPLE_SIZE - 1] = this->reading;
+
+      /*=== get smoothed reading ===*/
+      uint32_t smoothSum = 0;
+      for (size_t i = 0; i < AI_SMOOTHING_SAMPLE_SIZE; i++) {
+        smoothSum += this->readinHistory[i];
+      }
+      this->smoothedReading = smoothSum / AI_SMOOTHING_SAMPLE_SIZE;
+    } else {
+      uint32_t avg = 0;
+      for (size_t i = 0; i < 64; i++) {
+        avg += analogRead(this->pin);
+      };
+      this->smoothedReading = (avg / 64);
     }
-
-    /*=== Update Reading ===*/
-    this->reading = sum >> AI_SHIFT_BITS;
-
-    /*=== Update Reading History ===*/
-    for (size_t i = 1; i < AI_SMOOTHING_SAMPLE_SIZE; i++) {
-      this->readinHistory[i - 1] = this->readinHistory[i];
-    }
-    this->readinHistory[AI_SMOOTHING_SAMPLE_SIZE - 1] = this->reading;
-
-    /*=== get smoothed reading ===*/
-    uint32_t smoothSum = 0;
-    for (size_t i = 0; i < AI_SMOOTHING_SAMPLE_SIZE; i++) {
-      smoothSum += this->readinHistory[i];
-    }
-    this->smoothedReading = smoothSum / AI_SMOOTHING_SAMPLE_SIZE;
   }
 
   uint16_t getValue() {
-    this->value = map(constrain((int32_t)this->smoothedReading, 0, 16063), 0, 16063, 0, 4095);
+    if (this->resolution == AI_RESOLUTION_4096) {
+      this->value = map(constrain((int32_t)this->smoothedReading, 0, 16063), 0, 16063, 0, 4095);
+    } else {
+      this->value = constrain(this->smoothedReading, 0, 1023);
+    }
+
     return this->value;
   }
 };

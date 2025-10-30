68340a1a5605336ece023d3f7d871b0c12bd73ae
t <a@a.a>
2025-10-30 15:28:58 +0800

    feat: update

--- prod_nbiot_core/prod_nbiot_core.ino
+++ prod_nbiot_core/prod_nbiot_core.ino
@@ -40,7 +40,7 @@ void setup() {
   nbiotSerialRecv.reserve(128);
   nbiotPubMsgPayload.reserve(256);
   nbiotPubMsgCommand.reserve(512);
-  bool rubbishStrRes = rubbishStr.reserve(1024);  // push it to limit (variable amount)
+  bool rubbishStrRes = rubbishStr.reserve(1141);  // push it to limit (variable amount)
   Serial.print(rubbishStrRes ? F("[Str Space OK]") : F("[String Space NOT OK]"));
 
   /*=== NBIoT ===*/

f1b350c02d79cf5ff237a8ee548d65dfef802787
t <a@a.a>
2025-11-07 17:29:36 +0800

    feat: update

--- prod_nbiot_pqm_edition/edition/SubSystem.h
+++ prod_nbiot_pqm_edition/edition/SubSystem.h
@@ -1,13 +1,27 @@
+#include <ArduinoRS485.h>
+#include <ArduinoModbus.h>
+#include "./PowerMeter.h"
+#include "./SubGlobals.h"
+
 #ifndef SubSystem_H
 #define SubSystem_H
+
+PowerMeter powerMeter(100);
 
 class SubSystem {
 public:
   SubSystem(void) {}
 
-  void init() {}
-  
-  void loop() {}
+  void init() {
+    if (!mbClient.begin(9600)) {
+      Serial.println("Failed to start Modbus RTU Client!");
+      while (1) {};
+    }
+  }
+
+  void loop() {
+    powerMeter.loop();
+  }
 
   ~SubSystem() {}
 };

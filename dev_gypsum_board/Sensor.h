62284b84a4434a7eec630595d0f54e6785f6008e
t <a@a.a>
2025-11-10 13:49:14 +0800

    feat: update

---
+++ dev_gypsum_board/Sensor.h
@@ -0,0 +1,33 @@
+#include "U8g2lib.h"
+
+#ifndef sensor_h
+#define sensor_h
+
+
+class Sensor {
+private:
+  byte pin;
+  uint16_t reading = 0;
+
+  uint16_t avgRead() {
+    uint16_t avg = 0;
+    for (size_t i = 0; i < 64; i++) {
+      avg += analogRead(this->pin);
+    };
+    return avg / 64;
+  }
+
+public:
+  Sensor(byte pin)
+    : pin(pin) {
+    pinMode(this->pin, INPUT);
+  }
+
+  void loop() {
+    this->reading = this->avgRead();
+  }
+
+  ~Sensor(){};
+};
+
+#endif
--- dev_gypsum_board/dev_gypsum_board.ino
+++ dev_gypsum_board/dev_gypsum_board.ino
@@ -1,15 +1,25 @@
 #include "DisplayOLED.h"
+#include "Sensor.h"
 
 DisplayOLED displayOLED;
+Sensor s1(A0);
+Sensor s2(A1);
+Sensor s3(A2);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(A0, INPUT);
   displayOLED.init();
 }
 
 void loop() {
-  Serial.println(1023 - analogRead(A0));
-  displayOLED.draw(random(0, 1023), random(0, 1023), random(0, 1023));
+  s1.loop();
+  s2.loop();
+  s3.loop();
+
+  uint16_t r1 = 1023 - s1.reading;
+  uint16_t r2 = 1023 - s2.reading;
+  uint16_t r3 = 1023 - s3.reading;
+  
+  displayOLED.draw(r1, r2, r3);
   delay(1000);
 }

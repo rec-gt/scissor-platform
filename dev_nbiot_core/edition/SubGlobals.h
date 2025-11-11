136229e3883892129d2fba292ccdfb2ca10866d9
t <a@a.a>
2025-11-11 17:02:01 +0800

    feat: update

--- dev_nbiot_core/edition/SubGlobals.h
+++ dev_nbiot_core/edition/SubGlobals.h
@@ -4,8 +4,8 @@
 #include <ArduinoRS485.h>
 #include <ArduinoModbus.h>
 
-#define RS485Serial Serial3;
-#define RS485_RE_DE_PIN 22;
+#define RS485Serial Serial3
+#define RS485_RE_DE_PIN 22
 
 /*=== For Modbus ===*/
 RS485Class rs485(RS485Serial, 0, 0, RS485_RE_DE_PIN);

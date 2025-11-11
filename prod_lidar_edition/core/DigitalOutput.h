6112e5853142ebdf1c2663a11ef4703f95ed263f
t <a@a.a>
2025-11-11 16:00:53 +0800

    feat: update

---
+++ prod_lidar_edition/core/DigitalOutput.h
@@ -0,0 +1,38 @@
+#ifndef DigitalOutput_H
+#define DigitalOutput_H
+
+class DigitalOutput {
+private:
+  byte pin;
+  bool prevState = false;  // true = connect, false = cut
+
+public:
+  DigitalOutput() {}
+
+  DigitalOutput(byte pin)
+    : pin(pin) {
+    pinMode(pin, OUTPUT);
+    this->cut();
+  }
+
+  void control(bool state) {
+    if (state != this->prevState) {
+      digitalWrite(this->pin, state ? HIGH : LOW);
+      this->prevState = state;
+    }
+  }
+
+  void connect() {
+    this->control(true);
+  }
+
+  void cut() {
+    this->control(false);
+  }
+
+  bool getState() {
+    return this->prevState;
+  }
+};
+
+#endif

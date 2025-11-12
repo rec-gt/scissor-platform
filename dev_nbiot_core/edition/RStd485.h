e7f9f9eaa131c3d45e01b423bc33f70c241d1dfe
t <a@a.a>
2025-11-12 10:38:04 +0800

    feat: update

--- dev_nbiot_core/edition/RStd485.h
+++ dev_nbiot_core/edition/RStd485.h
@@ -9,7 +9,9 @@ public:
   RStd485(void) {}
 
   void init() {
-    recdStd485.begin(9600);
+    pinMode(RS485_RE_DE_PIN, OUTPUT);
+    digitalWrite(RS485_RE_DE_PIN, LOW);  // HIGH = send, LOW = receive
+    RS485Serial.begin(9600, SERIAL_8N1);
   }
 
   void loop() {
@@ -17,8 +19,10 @@ public:
   }
 
   void listen() {
-    while (recdStd485.available()) {
-      char c = recdStd485.read();
+    digitalWrite(RS485_RE_DE_PIN, LOW);
+    delay(1);
+    while (RS485Serial.available()) {
+      char c = RS485Serial.read();
 
       Serial.print(c);
 
@@ -26,10 +30,20 @@ public:
         rs485SerialRecv += c;
       }
       if (c == '\r') {
-        RS485.beginTransmission();
-        RS485.println(rs485SerialRecv);
-        RS485.endTransmission();
+        this->answer();
       }
+    }
+  }
+
+  void answer() {
+    cmpStr = F("AT");
+    int idx = rs485SerialRecv.indexOf(cmpStr);
+
+    if (idx > -1) {
+      digitalWrite(RS485_RE_DE_PIN, HIGH);
+      delay(1);
+      RS485Serial.println(F("[Hello from RGT Hello from RGT Hello from RGT]"));
+      RS485Serial.flush();
     }
   }
 

3dfe97c6657a1c26db91fe2e230b204400074b94
t <a@a.a>
2025-11-12 11:31:12 +0800

    feat: update

--- prod_nbiot_pqm_edition/edition/PowerMeter.h
+++ prod_nbiot_pqm_edition/edition/PowerMeter.h
@@ -47,8 +47,8 @@ public:
   void loop() {
     if (millis() - this->prevMillis >= 1000) {
       this->readIn1000ms();
-      // this->handlePublishContent();
-      // this->showData();
+      this->handlePublishContent();
+      this->showData();
 
       this->prevMillis = millis();
     }

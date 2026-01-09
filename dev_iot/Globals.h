624c52f41080ff4e81f1e61b713618256b43419b
t <a@a.a>
2026-01-09 15:31:02 +0800

    feat: update

---
+++ dev_iot/Globals.h
@@ -0,0 +1,78 @@
+#include "Lock.h"
+#include "Counter.h"
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+/*=== IoT ===*/
+#define SerialIoT Serial1
+#define IOT_MODULE_RESET_PIN 24
+
+enum IOT_STATE {
+  IOT_STATE_WAITING_INIT,
+
+  IOT_STATE_WAITING_RESET,
+  IOT_STATE_WAITING_RESET_HARDWARE,
+  IOT_STATE_FINISH_RESET_HARDWARE,
+  IOT_STATE_WAITING_RESET_SOFTWARE,
+  IOT_STATE_FINISH_RESET_SOFTWARE,
+  IOT_STATE_FINISH_RESET,
+
+  IOT_STATE_WAITING_IP,
+  IOT_STATE_FINISH_IP,
+  IOT_STATE_WAITING_CONFIG,
+  IOT_STATE_FINISH_CONFIG,
+  IOT_STATE_WAITING_CSQ,
+  IOT_STATE_FINISH_CSQ,
+  IOT_STATE_WAITING_CGATT,
+  IOT_STATE_FINISH_CGATT,
+  IOT_STATE_WAITING_CEREG,
+  IOT_STATE_FINISH_CEREG,
+  IOT_STATE_WAITING_OPEN_MQTT,
+  IOT_STATE_FINISH_OPEN_MQTT,
+  IOT_STATE_WAITING_CONN_MQTT,
+  IOT_STATE_FINISH_CONN_MQTT,
+  IOT_STATE_WAITING_SUBS_MQTT_TOPIC,
+  IOT_STATE_FINISH_SUBS_MQTT_TOPIC,
+
+  IOT_STATE_FINISH_INIT,
+
+  IOT_PIPELINE_INIT,
+  IOT_PIPELINE_WAITING_PREPARE_PUBMSG,
+  IOT_PIPELINE_FINISH_PREPARE_PUBMSG,
+  IOT_PIPELINE_WAITING_PUBLISH,
+  IOT_PIPELINE_FINISH_PUBLISH,
+};
+
+byte iotConnState = 0;
+String iotSerialRecv = "";
+String iotExtractedRecv = "";
+
+String iotCmpStr = "";
+int iotCmpStrIdx = 0;
+
+String iotCSQ = "";
+byte iotCSQTrial = 0;
+String iotIMEI = "";
+String iotCGATT = "";
+String iotCEREG = "";
+byte iotQueryCnt = 0;
+
+/*=== IoT MQTT ===*/
+String mqttConnCmd = "";
+String mqttSubsCmd = "";
+
+Lock mqttPublishLock;
+String mqttPublMsgPrepare = "";
+String mqttPublMsgPayload = "";
+String mqttSubsMsgContent = "";
+
+bool iotDebugMode = false;
+bool forcePublishMode = false;
+
+Counter iotCSQErrCnt;
+Counter iotCGATTErrCnt;
+Counter iotCEREGErrCnt;
+Counter iotPublishErrCnt;
+
+#endif
---
+++ dev_iot/dev_iot.ino
@@ -0,0 +1,43 @@
+#include "Globals.h"
+#include "IoT.h"
+
+IoT iot;
+
+AsyncTimer timer(1000);
+
+void setup() {
+  Serial.begin(9600);
+  iot.init();
+}
+
+String cmd = "";
+
+void serialInput() {
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+    if (c != '\r' && c != '\n') {
+      cmd += c;
+    }
+    if (c == '\r') {
+      if (cmd == "RESET") {
+        iotConnState = IOT_STATE_WAITING_INIT;
+      } else if (cmd == "FORCE") {
+        Serial.print("force publish");
+        iot.forcePublish();
+      } else {
+        iot.printlnFlush(cmd);
+      }
+      cmd = "";
+    }
+  }
+}
+
+void loop() {
+  serialInput();
+
+  iot.loop();
+
+  if (timer.asyncDelay(1000)) {
+    iot.buildMsg();
+  }
+}

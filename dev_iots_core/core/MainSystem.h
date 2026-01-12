fc0f365a8b13feca4e0e79402b715e5b410afa28
t <a@a.a>
2026-01-12 17:52:15 +0800

    feat: update

--- dev_iots_core/core/MainSystem.h
+++ dev_iots_core/core/MainSystem.h
@@ -55,7 +55,8 @@ public:
     /*=== AI Payload ===*/
     AIPayload = F("[");
     for (size_t i = 0; i < AI_NUMS; i++) {
-      AIPayload += analogInputs[i].getValue();
+      // AIPayload += analogInputs[i].getValue();
+      AIPayload += random(0, 1023);
       if (i < AI_NUMS - 1) {
         AIPayload += F(",");
       }

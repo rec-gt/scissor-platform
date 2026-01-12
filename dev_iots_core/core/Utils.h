7c810dcec004ab866ab442a57d93f8cb917a8400
t <a@a.a>
2026-01-12 10:05:23 +0800

    feat: update

---
+++ dev_iots_core/core/Utils.h
@@ -0,0 +1,54 @@
+#ifndef Utils_H
+#define Utils_H
+
+class Utils {
+public:
+  Utils(void){};
+
+  bool isNumeric(const String& str) {
+    for (size_t i = 0; i < str.length(); i++) {
+      if (!isDigit(str.charAt(i))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  byte hexCharToByte(char c) {
+    if (c >= '0' && c <= '9') {
+      return c - '0';
+    } else if (c >= 'A' && c <= 'F') {
+      return 10 + (c - 'A');
+    } else if (c >= 'a' && c <= 'f') {
+      return 10 + (c - 'a');
+    } else {
+      return 0;
+    }
+  }
+
+  void serialInput() {
+    while (Serial.available() > 0) {
+      char c = Serial.read();
+      if (c != '\r' && c != '\n') {
+        serialInputCmd += c;
+      }
+      if (c == '\r') {
+        if (serialInputCmd == F("RESET")) {
+          iotConnState = IOT_STATE_WAITING_INIT;
+        } else if (serialInputCmd == F("FORCE")) {
+          Serial.print(F("Force Publish"));
+          iot.forcePublish();
+        } else {
+          iot.printlnFlush(serialInputCmd);
+        }
+        serialInputCmd = F("");
+      }
+    }
+  }
+
+  ~Utils(){};
+};
+
+extern Utils utils;
+
+#endif
---
+++ dev_iots_core/dev_iots_core.ino
@@ -0,0 +1,78 @@
+#include "./core/MainSystem.h"
+#include "./core/IoT.h"
+#include "./core/DisplayClient.h"
+#include "./core/Utils.h"
+#include "./core/Timer.h"
+#include "./edition/SubSystem.h"
+#include <avr/wdt.h>
+
+MainSystem mainSystem;
+
+IoT iot;
+
+DisplayClient displayClient;
+
+Utils utils;
+
+SubSystem subSystem;
+
+Timer systemTimer(86400000);
+
+void setup() {
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+
+  /*=== String / Heap Memory Management ===*/
+  iotCSQ.reserve(8);
+  iotCGATT.reserve(8);
+  iotCEREG.reserve(8);
+  mqttSubsMsgContent.reserve(8);
+  iotIMEI.reserve(32);
+  iotExtractedRecv.reserve(32);
+  AOPayload.reserve(64);
+  mqttConnCmd.reserve(64);
+  mqttSubsCmd.reserve(64);
+  mqttPublMsgPrepare.reserve(128);
+  AIPayload.reserve(128);
+  rs485SerialRecv.reserve(128);
+  serialInputCmd.reserve(128);
+  iotSerialRecv.reserve(256);
+  mqttPublMsgPayload.reserve(256);
+  bool remainStrRes = debugStr.reserve(257);
+  Serial.print(remainStrRes ? F("[Str Space OK]") : F("[String Space NOT OK]"));
+
+  /*=== IoT ===*/
+  iot.init();
+
+  /*=== Display ===*/
+  displayClient.setup();
+
+  /*=== SubSystem Init ===*/
+  subSystem.init();
+
+  /*=== Watchdog ===*/
+  // wdt_enable(WDTO_8S);
+}
+
+void loop() {
+  utils.serialInput();
+
+  /*=== Register IoT ===*/
+  iot.loop();
+
+  /*=== Register MainSystem ===*/
+  mainSystem.loop();
+
+  /*=== Register display ===*/
+  displayClient.loop();
+
+  /*=== Register subSystem ===*/
+  subSystem.loop();
+
+  /*=== Pet the dog ===*/
+  // if (!systemTimer.isExpired()) {
+  //   wdt_reset();
+  // }
+
+  delay(10);
+}

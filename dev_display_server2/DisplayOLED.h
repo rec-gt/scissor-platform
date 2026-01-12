0f67665136273c90deb521f6021dbca4477fa909
t <a@a.a>
2026-01-12 15:09:24 +0800

    feat: update

--- dev_display_server2/DisplayOLED.h
+++ dev_display_server2/DisplayOLED.h
@@ -63,7 +63,7 @@ public:
       }
 
       if (hasIMEI) {
-        u8g2.print(F("RGT IoT"));
+        u8g2.print(F("RGT IoT V2"));
         u8g2.setCursor(53, y);
         u8g2.print(imei);
       } else {

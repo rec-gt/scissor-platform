5d4a52e5be268f01048a0de92f81fbc68232db52
t <a@a.a>
2026-01-14 15:24:13 +0800

    feat: update

--- dev_iots_core_test/core/IoTs.h
+++ dev_iots_core_test/core/IoTs.h
@@ -8,7 +8,7 @@ private:
   void listen() {
     while (SerialIoT.available() > 0) {
       char c = SerialIoT.read();
-      // Serial.print(c);
+      Serial.print(c);
       iotSerialRecv += c;
     }
   }

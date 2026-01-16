0197fb8b71f2d596270a8eff75eca953024ae278
t <a@a.a>
2026-01-16 17:02:40 +0800

    feat: update

--- dev_gypsum_board/dev_gypsum_board.ino
+++ dev_gypsum_board/dev_gypsum_board.ino
@@ -16,9 +16,13 @@ void loop() {
   s2.loop();
   s3.loop();
 
-  uint16_t r1 = map(constrain(1023 - s1.reading, 0, 1023), 0, 1023, 0, 1000);
-  uint16_t r2 = map(constrain(1023 - s2.reading, 0, 1023), 0, 1023, 0, 1000);
-  uint16_t r3 = map(constrain(1023 - s3.reading, 0, 1023), 0, 1023, 0, 1000);
+  // uint16_t r1 = map(constrain(1023 - s1.reading, 0, 1023), 0, 1023, 0, 1000);
+  // uint16_t r2 = map(constrain(1023 - s2.reading, 0, 1023), 0, 1023, 0, 1000);
+  // uint16_t r3 = map(constrain(1023 - s3.reading, 0, 1023), 0, 1023, 0, 1000);
+
+  uint16_t r1 = constrain(1023 - s1.reading, 0, 1023);
+  uint16_t r2 = constrain(1023 - s2.reading, 0, 1023);
+  uint16_t r3 = constrain(1023 - s3.reading, 0, 1023);
 
   displayOLED.draw(r1, r2, r3);
   delay(500);

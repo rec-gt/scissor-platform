f342120df987c85b583f16e6ae2ebd67afe55ffd
t <a@a.a>
2026-01-19 18:04:26 +0800

    feat: update

--- dev_iot_edition_scissor_platform/core/MainSystem.h
+++ dev_iot_edition_scissor_platform/core/MainSystem.h
@@ -55,8 +55,8 @@ public:
     /*=== AI Payload ===*/
     AIPayload = F("[");
     for (size_t i = 0; i < AI_NUMS; i++) {
-      // AIPayload += analogInputs[i].getValue();
-      AIPayload += random(0, 1023);
+      AIPayload += analogInputs[i].getValue();
+      // AIPayload += random(0, 1023);
       if (i < AI_NUMS - 1) {
         AIPayload += F(",");
       }

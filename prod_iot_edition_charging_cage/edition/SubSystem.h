2bcb7aae51525829bd711722a975ea24107fa73b
t <a@a.a>
2026-01-23 12:28:44 +0800

    feat: update

--- prod_iot_edition_charging_cage/edition/SubSystem.h
+++ prod_iot_edition_charging_cage/edition/SubSystem.h
@@ -1,14 +1,22 @@
 #ifndef SubSystem_H
 #define SubSystem_H
+#define HISTORY_SIZE 10
 
 #include "./SubGlobals.h"
 
-int temp1 = 2500;
-int temp2 = 2500;
-int temp3 = 2500;
-int temp4 = 2500;
-int temp5 = 2500;
-int temp6 = 2500;
+int temp1 = 25;
+int temp2 = 25;
+int temp3 = 25;
+int temp4 = 25;
+int temp5 = 25;
+int temp6 = 25;
+
+int tempHistory1[10] = {};
+int tempHistory2[10] = {};
+int tempHistory3[10] = {};
+int tempHistory4[10] = {};
+int tempHistory5[10] = {};
+int tempHistory6[10] = {};
 
 DigitalOutput &relay1 = digitalOutputs[0];
 DigitalOutput &relay2 = digitalOutputs[1];
@@ -28,6 +36,13 @@ private:
 
   bool isOverHeat() {
     return (temp1 > 60 || temp2 > 60 || temp3 > 60 || temp4 > 60 || temp5 > 60 || temp6 > 60);
+  }
+
+  void updateTempHistory(int (&history)[HISTORY_SIZE], int temp) {
+    for (int i = 1; i < HISTORY_SIZE; i++) {
+      history[i - 1] = history[i];
+    }
+    history[HISTORY_SIZE - 1] = temp;
   }
 
 public:
@@ -51,6 +66,13 @@ public:
     temp4 = readingToActualTemp(kps4.getValue());
     temp5 = readingToActualTemp(kps5.getValue());
     temp6 = readingToActualTemp(kps6.getValue());
+
+    this->updateTempHistory(tempHistory1, temp1);
+    this->updateTempHistory(tempHistory2, temp2);
+    this->updateTempHistory(tempHistory3, temp3);
+    this->updateTempHistory(tempHistory4, temp4);
+    this->updateTempHistory(tempHistory5, temp5);
+    this->updateTempHistory(tempHistory6, temp6);
 
     if (this->isOverHeat()) {
       relay1.cut();

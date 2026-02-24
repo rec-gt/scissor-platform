3b6b0a04b43418bc03845a3002732fab01445e28
t <a@a.a>
2026-02-24 17:52:42 +0800

    feat: update

---
+++ prod_iot_edition_gypsum_board/edition/Sensor.h
@@ -0,0 +1,31 @@
+#ifndef sensor_h
+#define sensor_h
+
+class Sensor {
+private:
+  byte pin;
+
+  uint16_t avgRead() {
+    uint16_t avg = 0;
+    for (size_t i = 0; i < 64; i++) {
+      avg += analogRead(this->pin);
+    };
+    return avg / 64;
+  }
+
+public:
+  uint16_t reading = 0;
+
+  Sensor(byte pin)
+    : pin(pin) {
+    pinMode(this->pin, INPUT);
+  }
+
+  void loop() {
+    this->reading = this->avgRead();
+  }
+
+  ~Sensor(){};
+};
+
+#endif
--- prod_iot_edition_gypsum_board/edition/SubSystem.h
+++ prod_iot_edition_gypsum_board/edition/SubSystem.h
@@ -3,8 +3,14 @@
 
 #include "./SubGlobals.h"
 #include "./SubRS485.h"
+#include "./Sensor.h"
 
 SubRS485 subRS485;
+
+Sensor s1(A0);
+Sensor s2(A1);
+Sensor s3(A2);
+Sensor s4(A3);
 
 class SubSystem {
 public:
@@ -16,7 +22,13 @@ public:
   }
 
   void loop() {
-    // subRS485.loop();
+    s1.loop();
+    s2.loop();
+
+    uint16_t r1 = map(340 - constrain(s1.reading, 0, 340), 0, 340, 0, 1023);
+    uint16_t r2 = map(340 - constrain(s2.reading, 0, 340), 0, 340, 0, 1023);
+
+    Serial.println(r1);
   }
 
   ~SubSystem() {}

911c0ea20e61db3a948357fea18189a313bbc3ff
t <a@a.a>
2026-02-25 10:51:10 +0800

    feat: update

---
+++ dev_cmp_test/edition/SubSystem.h
@@ -0,0 +1,47 @@
+#ifndef SubSystem_H
+#define SubSystem_H
+
+#include "./SubGlobals.h"
+#include "./SubRS485.h"
+#include "../core/Iot.h"
+
+SubRS485 subRS485;
+Timer alarmTimer;
+
+class SubSystem {
+public:
+  bool canPublish = true;
+  byte didata = 0;
+
+  SubSystem(void) {}
+
+  void init() {
+    configAnalogInputResolution(0);
+    subRS485.init();
+  }
+
+  void loop() {
+    subRS485.loop();
+    if (digitalInputs[0].getState() == 1) {
+      if (canPublish) {
+        didata |= 1 << 4;
+        didata |= 1 << 5;  // X000 1100
+        didata |= 0 << 6;
+        didata |= 0 << 7;
+
+        iot.buildMsg(didata, 0, F(""), F(""));
+        iot.forcePublish();
+        alarmTimer.refresh();
+        canPublish = false;
+      }
+    }
+
+    if (alarmTimer.autoTimeout(10000)) {
+      canPublish = true;
+    }
+  }
+
+  ~SubSystem() {}
+};
+
+#endif

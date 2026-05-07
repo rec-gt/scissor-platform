4ff9a4465afec7ee518bbfb4eec1155cd217f813
t <a@a.a>
2026-05-07 09:47:21 +0800

    feat: update

--- prod_iot_edition_cet/edition/SubGlobals.h
+++ prod_iot_edition_cet/edition/SubGlobals.h
@@ -19,5 +19,9 @@ void configAnalogInputResolution(bool r = 0) {  // 0 = 1024, 1 = 4096
 RS485Class recommendedStandard485(RS485Serial, 0, 0, RS485_RE_DE_PIN);
 ModbusRTUClientClass mbRtuClient(recommendedStandard485);
 // ModbusRTUServerClass mbServer(recommendedStandard485);
+Counter modbusCounter;
+
+/*=== For sub system ===*/
+Timer deviceTimer;
 
 #endif

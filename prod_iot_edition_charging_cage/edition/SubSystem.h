9614da5989d0d55b5ab7b531e71dbc206bf26fb7
t <a@a.a>
2026-05-15 15:03:19 +0800

    feat: update

--- prod_iot_edition_charging_cage/edition/SubSystem.h
+++ prod_iot_edition_charging_cage/edition/SubSystem.h
@@ -110,8 +110,8 @@ private:
   void overwriteMQTTPayloads() {
     /*=== DI Payload ===*/
     DIPayload = 0;
-    DIPayload |= (sysStatus == SUBSYS_STOPPED ? 1 : 0) << 0;
-    DIPayload |= (sysHealth == SUBSYS_FAILURE ? 1 : 0) << 1;
+    DIPayload |= (this->isStatus(SUBSYS_STOPPED) ? 1 : 0) << 0;
+    DIPayload |= (this->isStatus(SUBSYS_FAILURE) ? 1 : 0) << 1;
     DIPayload |= 0 << 2;
     DIPayload |= 0 << 3;
     DIPayload |= 0 << 4;

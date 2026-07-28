75e3bbf1ce8d293587e9704f887f1c3e9de61aaf
t <a@a.a>
2026-07-28 10:11:20 +0800

    feat: update

---
+++ prod_iot_edition_modbus/edition/ModbusConfig.h
@@ -0,0 +1,87 @@
+#include <stdint.h>
+#include "./SubGlobals.h"
+
+#ifndef CET_H
+#define CET_H
+
+/*=== Derek, please config modbus here ===*/
+constexpr uint16_t SLAVE_ID = 1;
+constexpr uint16_t MB_BAUD_RATE = 9600;
+constexpr uint16_t MB_SERIAL_CONFIG = SERIAL_8N1;  // e.g., SERIAL_8E1, SERIAL_8N1
+constexpr uint8_t MB_DATA_SIZE = 100;
+constexpr uint8_t MB_DATA_BATCH_SIZE = 20;
+constexpr uint8_t MB_READ_EPOCH_MAX = (MB_DATA_SIZE + MB_DATA_BATCH_SIZE - 1) / MB_DATA_BATCH_SIZE;
+uint8_t mbReadEpochIdx = 0;
+uint32_t mbDataHr[MB_DATA_SIZE] = {};  // read first n HR data, where n = MB_DATA_SIZE
+
+/*=== End of config ===*/
+
+class CET {
+private:
+  float IEEEfloat(uint32_t val) {
+    union {
+      uint32_t i;
+      float f;
+    } u;
+    u.i = val;
+    return u.f;
+  }
+
+public:
+  CET(void) {}
+
+  void init() {
+    if (!mbRtuClient.begin(MB_BAUD_RATE, MB_SERIAL_CONFIG)) {
+      Serial.println(F("Failed to start Modbus RTU Client!"));
+      while (1) {};
+    }
+  }
+
+  void loop() {
+    if (deviceTimer.autoTimeout(1000)) {
+      this->readModbusData();
+      this->debug();
+    }
+  }
+
+  void readModbusData(void) {
+    if (mbReadEpochIdx > MB_READ_EPOCH_MAX) {
+      mbReadEpochIdx = 0;
+      return;
+    }
+
+    mbReadEpochIdx++;
+
+    uint8_t startIdx = mbReadEpochIdx * MB_DATA_BATCH_SIZE;  // e.g., 0, 20, 40, 60, ...
+    uint8_t endIdx = startIdx + MB_DATA_BATCH_SIZE + 1;      // e.g., 21, 41, 61, 81, ...
+    endIdx = endIdx >= MB_DATA_SIZE ? MB_DATA_SIZE : endIdx;
+
+    mbRtuClient.requestFrom(SLAVE_ID, HOLDING_REGISTERS, startIdx, MB_DATA_BATCH_SIZE);
+
+    for (uint8_t i = startIdx; i < endIdx; i++) {
+      mbHrData[i] = mbRtuClient.read()
+    }
+  }
+
+  // void readModbusData() {
+  //   mbRtuClient.requestFrom(SLAVE_ID, HOLDING_REGISTERS, 0, 6);
+  //   holdingRegisterValues[0] = IEEEfloat(((uint32_t)mbRtuClient.read() << 16) | mbRtuClient.read());
+  //   holdingRegisterValues[1] = IEEEfloat(((uint32_t)mbRtuClient.read() << 16) | mbRtuClient.read());
+  //   holdingRegisterValues[2] = IEEEfloat(((uint32_t)mbRtuClient.read() << 16) | mbRtuClient.read());
+
+  //   mbRtuClient.requestFrom(SLAVE_ID, HOLDING_REGISTERS, 46, 2);
+  //   holdingRegisterValues[3] = (((uint32_t)mbRtuClient.read() << 16) | mbRtuClient.read()) * 0.01;
+  // }
+
+  void debug() {
+    Serial.println(F("\r\n>>> Print Data\r\n"));
+
+    for (byte i = 0; i < MB_DATA_SIZE; i++) {
+      Serial.print(i);
+      Serial.print(F(": "));
+      Serial.println(mbHrData[i], 4);
+    }
+  }
+};
+
+#endif
